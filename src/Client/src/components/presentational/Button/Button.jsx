3de7aa8476e7e834ae2cc6ad95911c9333ac5083
Spec <dawidjachowicz91@gmail.com>
2020-01-12 12:54:30 +0100

    Add Button component

---
+++ src/Client/src/components/presentational/Button/Button.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Button = (props) => {
+    return (
+        <button
+            type="button"
+            className={"nes-btn " + props.type}
+            onClick={() => props.onClick()}>
+            {props.children}
+        </button>
+    );
+};
+
+Button.types = {
+    PRIMARY: "is-primary",
+    SUCCESS: "is-success",
+    WARNING: "is-warning",
+    ERROR: "is-error",
+    DISABLED: "is-disabled"
+}
+
+Button.propTypes = {
+    children: PropTypes.any,
+    type: PropTypes.oneOf(Object.values(Button.types)).isRequired,
+    onClick: PropTypes.func.isRequired
+};
+
+export default Button;
