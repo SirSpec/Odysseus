339e917f20b78f9798bab7ac7ee414844354ed69
Spec <dawidjachowicz91@gmail.com>
2020-01-12 12:52:49 +0100

    Add Span component

---
+++ src/Client/src/components/presentational/Span/Span.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Span = (props) => {
+    return (
+        <span className={"nes-text " + props.type}>
+            {props.children}
+        </span>
+    );
+};
+
+Span.types = {
+    PRIMARY: "is-primary",
+    SUCCESS: "is-success",
+    WARNING: "is-warning",
+    ERROR: "is-error",
+    DISABLED: "is-disabled"
+}
+
+Span.propTypes = {
+    children: PropTypes.any,
+    type: PropTypes.oneOf(Object.values(Span.types)).isRequired
+};
+
+export default Span;
