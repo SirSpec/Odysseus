f3d160c9143a24d818b4f150a42d0fb9def806e4
Spec <dawid.spec.jachowicz@gmail.com>
2020-03-02 23:49:58 +0100

    Add Size struct and corresponding Unit Tests

---
+++ src/server/MapGenerator/Size.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Theseus.MapGenerator
+{
+    public readonly struct Size : IEquatable<Size>
+    {
+        public int Width { get; }
+        public int Height { get; }
+
+        public Size(int width, int height) => (Width, Height) = (width, height);
+        public void Deconstruct(out int width, out int height) => (width, height) = (Width, Height);
+
+        public bool Equals(Size other) => Width == other.Width && Height == other.Height;
+    }
+}
