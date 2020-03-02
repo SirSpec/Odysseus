1d003c784af3ae72126021c9b778942850c16181
Spec <dawidjachowicz91@gmail.com>
2020-03-02 23:53:14 +0100

    Add Vector struct and corresponding Unit Tests

---
+++ src/server/MapGenerator/Vector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Theseus.MapGenerator
+{
+    public readonly struct Vector : IEquatable<Vector>
+    {
+        public Tile Start { get; }
+        public Tile End { get; }
+
+        public Vector(Tile start, Tile end) => (Start, End) = (start, end);
+        public void Deconstruct(out Tile start, out Tile end) => (start, end) = (Start, End);
+
+        public bool Equals(Vector other) => Start.Equals(other.Start) && End.Equals(other.End);
+    }
+}
---
+++ test/MapGeneratorTest/VectorTest.cs
@@ -0,0 +1,97 @@
+using Theseus.MapGenerator;
+using Xunit;
+
+namespace Theseus.MapGeneratorTest
+{
+    public class VectorTest
+    {
+        [Fact]
+        public void DefaultContructor_EmptyObject_EmptyProperties()
+        {
+            //Arrange
+            var testObject = new Vector();
+
+            //Act
+            var (start, end) = testObject;
+
+            //Assert
+            Assert.Equal((0, 0), (start.X, start.Y));
+            Assert.Equal((0, 0), (end.X, end.Y));
+        }
+
+        [Fact]
+        public void ParameterizedConstructor_ValidValues_ValidProperties()
+        {
+            //Arrange
+            var testObject = new Vector(new Tile(1, 2), new Tile(3, 4));
+
+            //Act
+            var (start, end) = testObject;
+
+            //Assert
+            Assert.Equal((1, 2), (start.X, start.Y));
+            Assert.Equal((3, 4), (end.X, end.Y));
+        }
+
+        [Fact]
+        public void Equals_EmptyObjects_True()
+        {
+            //Arrange
+            var testObject1 = new Vector();
+            var testObject2 = new Vector();
+
+            //Act
+            var result1 = testObject1.Equals(testObject1);
+            var result2 = testObject1.Equals(testObject2);
+            var result3 = testObject2.Equals(testObject1);
+
+            //Assert
+            Assert.True(result1);
+            Assert.True(result2);
+            Assert.True(result3);
+        }
+
+        [Fact]
+        public void Equals_SameParameters_True()
+        {
+            //Arrange
+            var testObject1 = new Vector(new Tile(1, 2), new Tile(3, 4));
+            var testObject2 = new Vector(new Tile(1, 2), new Tile(3, 4));
+
+            //Act
+            var result1 = testObject1.Equals(testObject1);
+            var result2 = testObject1.Equals(testObject2);
+            var result3 = testObject2.Equals(testObject1);
+
+            //Assert
+            Assert.True(result1);
+            Assert.True(result2);
+            Assert.True(result3);
+        }
+
+        [Fact]
+        public void Equals_DifferentParameters_False()
+        {
+            //Arrange
+            var testObject1 = new Vector(new Tile(1, 2), new Tile(3, 4));
+            var testObject2 = new Vector(new Tile(1, 2), new Tile(3, 5));
+
+            var testObject3 = new Vector(new Tile(5, 2), new Tile(3, 4));
+            var testObject4 = new Vector(new Tile(6, 2), new Tile(3, 4));
+
+            //Act
+            var result1 = testObject1.Equals(testObject2);
+            var result2 = testObject2.Equals(testObject1);
+
+            var result3 = testObject3.Equals(testObject4);
+            var result4 = testObject4.Equals(testObject3);
+
+            //Assert
+            Assert.False(result1);
+            Assert.False(result2);
+
+            Assert.False(result3);
+            Assert.False(result4);
+        }
+    }
+}
