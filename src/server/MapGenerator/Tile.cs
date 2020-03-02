dbb3c0bbc1067d18319232677f7ce37aed617bc1
Spec <dawidjachowicz91@gmail.com>
2020-03-02 23:52:16 +0100

    Add Tile struct and corresponding Unit Tests

---
+++ src/server/MapGenerator/Tile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Theseus.MapGenerator
+{
+    public readonly struct Tile : IEquatable<Tile>
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Tile(int x, int y) => (X, Y) = (x, y);
+        public void Deconstruct(out int x, out int y) => (x, y) = (X, Y);
+
+        public bool Equals(Tile other) => X == other.X && Y == other.Y;
+    }
+}
