d4e16cfd7668246df8c34bbd6248f81335cffa01
Spec <dawidjachowicz91@gmail.com>
2020-03-02 23:53:43 +0100

    Add Corridor struct and corresponding Unit Tests

---
+++ src/server/MapGenerator/Corridor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Theseus.MapGenerator
+{
+    public readonly struct Corridor : IEquatable<Corridor>
+    {
+        public Vector Vector { get; }
+
+        public Corridor(Vector vector) => (Vector) = (vector);
+        public bool Equals(Corridor other) => Vector.Equals(other.Vector);
+    }
+}
