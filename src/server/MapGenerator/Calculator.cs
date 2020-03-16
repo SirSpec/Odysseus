8dfa9c12b16e6cb55079210da38ee6499dfece02
Spec <dawid.spec.jachowicz@gmail.com>
2020-03-16 22:22:52 +0100

    Add temporary Calculator static class  - Contains method that calculates Distance between vertices

---
+++ src/server/MapGenerator/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Theseus.MapGenerator
+{
+    public class Calculator
+    {
+        public static double Distance(Vertex a, Vertex b)
+        {
+            var deltaX = a.X - b.X;
+            var deltaY = a.Y - b.Y;
+
+            return Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+        }
+    }
+}
