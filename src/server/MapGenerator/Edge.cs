a9836bd53d265e049601e87de867a3b4797f6e5d
Spec <dawidjachowicz91@gmail.com>
2020-03-20 15:04:31 +0100

    Change and refactor Graph and Edge implementation  - Edge is directed  - Edge has better interface  - Graph has better interface  - Graph uses Directed Edge implementation  - Change Unit Tests accordingly

--- src/server/MapGenerator/Edge.cs
+++ src/server/MapGenerator/Edge.cs
@@ -4,26 +4,14 @@ namespace Theseus.MapGenerator
 {
     public readonly struct Edge : IEquatable<Edge>
     {
-        public (Vertex first, Vertex second) Vertices { get; }
+        public Vertex From { get; }
+        public Vertex To { get; }
         public double Weight { get; }
 
-        public Edge((Vertex first, Vertex second) vertices, double weight) =>
-            (Vertices, Weight) = (vertices, weight);
+        public Edge(Vertex from, Vertex to, double weight) =>
+            (From, To, Weight) = (from, to, weight);
 
-        public void Deconstruct(out (Vertex first, Vertex second) vertices, out double weight) =>
-            (vertices, weight) = (Vertices, Weight);
-
-        public bool Equals(Edge other)
-        {
-            var (first, second) = Vertices;
-            var (otherFirst, otherSecond) = other.Vertices;
-
-            return Weight == other.Weight &&
-                (first.Equals(otherFirst) || first.Equals(otherSecond)) &&
-                (second.Equals(otherFirst) || second.Equals(otherSecond));
-        }
-
-        public bool Contains(Vertex vertex) =>
-            Vertices.first.Equals(vertex) || Vertices.second.Equals(vertex);
+        public bool Equals(Edge other) =>
+            Weight == other.Weight && From.Equals(other.From) && To.Equals(other.To);
     }
 }
