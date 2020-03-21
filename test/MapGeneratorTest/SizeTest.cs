34bcdf2a46591cfcf6abfe5bc4cba1df67b500d5
Spec <dawidjachowicz91@gmail.com>
2020-03-21 12:03:35 +0100

    Refactor UnitTests  - Change variable name to SUT which stands for System Under Test

--- test/MapGeneratorTest/SizeTest.cs
+++ test/MapGeneratorTest/SizeTest.cs
@@ -9,10 +9,10 @@ namespace Theseus.MapGeneratorTest
         public void DefaultContructor_EmptyObject_EmptyProperties()
         {
             //Arrange
-            var testObject = new Size();
+            var sut = new Size();
 
             //Act
-            var (width, height) = testObject;
+            var (width, height) = sut;
 
             //Assert
             Assert.Equal(0, width);
@@ -23,10 +23,10 @@ namespace Theseus.MapGeneratorTest
         public void ParameterizedConstructor_ValidValues_ValidProperties()
         {
             //Arrange
-            var testObject = new Size(1, 2);
+            var sut = new Size(1, 2);
 
             //Act
-            var (width, height) = testObject;
+            var (width, height) = sut;
 
             //Assert
             Assert.Equal(1, width);
@@ -37,13 +37,13 @@ namespace Theseus.MapGeneratorTest
         public void Equals_EmptyObjects_True()
         {
             //Arrange
-            var testObject1 = new Size();
-            var testObject2 = new Size();
+            var sut1 = new Size();
+            var sut2 = new Size();
 
             //Act
-            var result1 = testObject1.Equals(testObject1);
-            var result2 = testObject1.Equals(testObject2);
-            var result3 = testObject2.Equals(testObject1);
+            var result1 = sut1.Equals(sut1);
+            var result2 = sut1.Equals(sut2);
+            var result3 = sut2.Equals(sut1);
 
             //Assert
             Assert.True(result1);
@@ -55,13 +55,13 @@ namespace Theseus.MapGeneratorTest
         public void Equals_SameParameters_True()
         {
             //Arrange
-            var testObject1 = new Size(1, 2);
-            var testObject2 = new Size(1, 2);
+            var sut1 = new Size(1, 2);
+            var sut2 = new Size(1, 2);
 
             //Act
-            var result1 = testObject1.Equals(testObject1);
-            var result2 = testObject1.Equals(testObject2);
-            var result3 = testObject2.Equals(testObject1);
+            var result1 = sut1.Equals(sut1);
+            var result2 = sut1.Equals(sut2);
+            var result3 = sut2.Equals(sut1);
 
             //Assert
             Assert.True(result1);
@@ -73,18 +73,18 @@ namespace Theseus.MapGeneratorTest
         public void Equals_DifferentParameters_False()
         {
             //Arrange
-            var testObject1 = new Size(1, 3);
-            var testObject2 = new Size(1, 2);
+            var sut1 = new Size(1, 3);
+            var sut2 = new Size(1, 2);
 
-            var testObject3 = new Size(1, 2);
-            var testObject4 = new Size(2, 2);
+            var sut3 = new Size(1, 2);
+            var sut4 = new Size(2, 2);
 
             //Act
-            var result1 = testObject1.Equals(testObject2);
-            var result2 = testObject2.Equals(testObject1);
+            var result1 = sut1.Equals(sut2);
+            var result2 = sut2.Equals(sut1);
 
-            var result3 = testObject3.Equals(testObject4);
-            var result4 = testObject4.Equals(testObject3);
+            var result3 = sut3.Equals(sut4);
+            var result4 = sut4.Equals(sut3);
 
             //Assert
             Assert.False(result1);
