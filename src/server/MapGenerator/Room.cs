5211ad82b70a1490ea0f35a9738cabab956f5eff
Spec <dawidjachowicz91@gmail.com>
2020-03-21 12:01:33 +0100

    Refactor Room struct  - Change name of TopLeftLocation property to TopLeft

--- src/server/MapGenerator/Room.cs
+++ src/server/MapGenerator/Room.cs
@@ -4,12 +4,12 @@ namespace Theseus.MapGenerator
 {
     public readonly struct Room : IEquatable<Room>
     {
-        public Tile TopLeftLocation { get; }
+        public Tile TopLeft { get; }
         public Size Size { get; }
 
-        public Room(Tile topLeftLocation, Size size) => (TopLeftLocation, Size) = (topLeftLocation, size);
-        public void Deconstruct(out Tile topLeftLocation, out Size size) => (topLeftLocation, size) = (TopLeftLocation, Size);
+        public Room(Tile topLeft, Size size) => (TopLeft, Size) = (topLeft, size);
+        public void Deconstruct(out Tile topLeftLocation, out Size size) => (topLeftLocation, size) = (TopLeft, Size);
 
-        public bool Equals(Room other) => TopLeftLocation.Equals(other.TopLeftLocation) && Size.Equals(other.Size);
+        public bool Equals(Room other) => TopLeft.Equals(other.TopLeft) && Size.Equals(other.Size);
     }
 }
