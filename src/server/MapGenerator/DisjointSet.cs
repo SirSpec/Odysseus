3a11518e58d89076de998b1d8056c355a839a31c
Spec <dawidjachowicz91@gmail.com>
2020-03-21 21:20:06 +0100

    Add DisjointSet data structure and corresponding Unit Tests

---
+++ src/server/MapGenerator/DisjointSet.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Theseus.MapGenerator
+{
+    // https://en.wikipedia.org/wiki/Disjoint-set_data_structure
+    public class DisjointSet<TValue> where TValue : notnull, IEquatable<TValue>
+    {
+        private readonly IList<(TValue Element, TValue Parent)> set;
+
+        public DisjointSet(IEnumerable<TValue> values)
+        {
+            set = values.Select(value => (value, value)).ToList();
+        }
+        
+        public TValue FindRoot(TValue element)
+        {
+            var parent = this[element];
+
+            if (!parent.Equals(element)) return FindRoot(parent);
+            else return element;
+        }
+
+        public void Union(TValue subsetElementA, TValue subsetElementB)
+        {
+            var subsetRootA = FindRoot(subsetElementA);
+            var subsetRootB = FindRoot(subsetElementB);
+            
+            this[subsetRootA] = subsetRootB;
+        }
+
+        private TValue this[TValue element]
+        {
+            get => set.First(setPair => setPair.Element.Equals(element)).Parent;
+            set
+            {
+                var setPair = set.First(setPair => setPair.Element.Equals(element));
+                var pairIndex = set.IndexOf(setPair);
+                setPair.Parent = value;
+                set.RemoveAt(pairIndex);
+                set.Insert(pairIndex, setPair);
+            }
+        }
+    }
+}
---
+++ test/MapGeneratorTest/DisjointSetTest.cs
@@ -0,0 +1,56 @@
+using Xunit;
+using Theseus.MapGenerator;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Theseus.MapGeneratorTest
+{
+    public class DisjointSetTest
+    {
+        private const string TestValue1 = "TestValue1";
+        private const string TestValue2 = "TestValue2";
+        private const string TestValue3 = "TestValue3";
+
+        private readonly IList<Vertex<string>> TestObjects = new Vertex<string>[]
+        {
+            new Vertex<string>(TestValue1),
+            new Vertex<string>(TestValue2),
+            new Vertex<string>(TestValue3)
+        };
+
+        [Fact]
+        public void FindRoot_InitialSet_EachElementInDifferentSubset()
+        {
+            //Arrange
+            var sut = new DisjointSet<Vertex<string>>(TestObjects);
+
+            //Act
+            var results = TestObjects.Select(sut.FindRoot);
+
+            //Assert
+            foreach (var testObject in TestObjects)
+            {
+                Assert.Contains(testObject, results);
+            }
+        }
+
+        [Fact]
+        public void Union_UnionOfTwoElements_TwoElelemntInTheSameSubset()
+        {
+            //Arrange
+            var sut = new DisjointSet<Vertex<string>>(TestObjects);
+
+            //Act
+            sut.Union(TestObjects[0], TestObjects[1]);
+
+            var resultRoot1 = sut.FindRoot(TestObjects[0]);
+            var resultRoot2 = sut.FindRoot(TestObjects[1]);
+            var equalsResult = resultRoot1.Equals(resultRoot2);
+
+            //Assert
+            Assert.True(equalsResult);
+            Assert.Equal(TestObjects[1], resultRoot1);
+            Assert.Equal(TestObjects[1], resultRoot2);
+        }
+    }
+}
