812b90447d454e779f8d7514fb06a664bc39621d
Spec <dawidjachowicz91@gmail.com>
2020-04-05 16:36:03 +0200

    Implement Offset struct and corresponding Unit Tests

---
+++ src/server/DomainModel/MapGenerator/Offset.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Odysseus.DomainModel.MapGenerator
+{
+    public readonly struct Offset : IEquatable<Offset>
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Offset(int x, int y) => (X, Y) = (x, y);
+
+        public void Deconstruct(out int x, out int y) => (x, y) = (X, Y);
+        public bool Equals(Offset other) => X == other.X && Y == other.Y;
+
+        public static Offset LeftBy(int offset) => new Offset(-offset, 0);
+        public static Offset RightBy(int offset) => new Offset(offset, 0);
+        public static Offset TopBy(int offset) => new Offset(0, offset);
+        public static Offset BottomBy(int offset) => new Offset(0, -offset);
+    }
+}
