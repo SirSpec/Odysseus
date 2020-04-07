7ec80b14a41751dede84f59bbf4139fef0af97ad
Spec <dawidjachowicz91@gmail.com>
2020-04-07 18:35:31 +0200

    Add Random numbers engine and corresponding Types and Unit Tests

---
+++ src/server/DomainModel/MapGenerator/ConstraintRange.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Odysseus.DomainModel.MapGenerator
+{
+    public readonly struct ConstraintRange<TValue> : IEquatable<ConstraintRange<TValue>>
+        where TValue : IComparable<TValue>, IEquatable<TValue>
+    {
+        public TValue Min { get; }
+        public TValue Max { get; }
+
+        public ConstraintRange(TValue min, TValue max)
+        {
+            if (min.CompareTo(max) == 1)
+                throw new ArgumentOutOfRangeException($"{nameof(min)}:{min} must be less than or equal to {nameof(max)}:{max}");
+
+            (Min, Max) = (min, max);
+        }
+
+        public void Deconstruct(out TValue min, out TValue max) => (min, max) = (Min, Max);
+        public bool Equals(ConstraintRange<TValue> other) =>
+            Min.Equals(other.Min) && Max.Equals(other.Max);
+    }
+}
---
+++ src/server/DomainModel/MapGenerator/IRandomNumberEngine.cs
@@ -0,0 +1,8 @@
+﻿namespace Odysseus.DomainModel.MapGenerator
+{
+    public interface IRandomNumberEngine
+    {
+        int RandomInteger(ConstraintRange<int> constraint);
+        double RandomDouble(ConstraintRange<double> constraint);
+    }
+}
