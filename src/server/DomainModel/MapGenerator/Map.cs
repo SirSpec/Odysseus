377495ad02a966f2f3634deca3cc39f92565efe7
Spec <dawidjachowicz91@gmail.com>
2020-04-09 21:08:58 +0200

    Implement prototype of Rooms Dungeon generator - Add Generators of Rooms and Corridors

---
+++ src/server/DomainModel/MapGenerator/Map.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Odysseus.DomainModel.MapGenerator
+{
+    public readonly struct Map
+    {
+        public IEnumerable<Tile> Tiles { get; }
+        
+        public Map(IEnumerable<Tile> tiles)
+        {
+            Tiles = tiles;
+        }
+    }
+}
