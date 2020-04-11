0dee7fb4263b1dc668e8b77e0bf8638dfea517f3
Spec <dawid.spec.jachowicz@gmail.com>
2020-04-11 15:29:28 +0200

    Add Anchor component stories

---
+++ src/client/src/components/Anchor/Anchor.stories.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Anchor from "./Anchor";
+import "../../styles/global.scss";
+
+export default { title: "Components/Anchor" };
+
+export const Default = () =>
+  <Anchor href="https://www.odysseus.com" >
+    Anchor
+  </Anchor>
