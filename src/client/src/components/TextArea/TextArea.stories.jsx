26f2c04de7ea930cb34a9aca4bf5c623b8a52f33
Spec <dawidjachowicz91@gmail.com>
2020-04-11 15:33:14 +0200

    Add TextArea component stories

---
+++ src/client/src/components/TextArea/TextArea.stories.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import TextArea from "./TextArea";
+import "../../styles/global.scss";
+
+export default { title: "Components/TextArea" };
+
+export const Default = () =>
+  <TextArea id="Default" label="TextArea label" />
