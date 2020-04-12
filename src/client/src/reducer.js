6a40b9ee64635593233c2fcfebe1dc3065168544
Spec <dawidjachowicz91@gmail.com>
2020-04-12 15:30:50 +0200

    Add prototype of Redux integration

---
+++ src/client/src/reducer.js
@@ -0,0 +1,25 @@
+import * as actions from './actions';
+
+function todos(state = [], action) {
+    switch (action.type) {
+        case actions.ADD_TODO:
+            return [
+                ...state,
+                {
+                    text: action.text,
+                    completed: false
+                }
+            ]
+        case actions.TOGGLE_TODO:
+            return state.map((todo, index) => {
+                if (index === action.index) {
+                    return { ...todo, completed: !todo.completed }
+                }
+                else return todo
+            })
+        default:
+            return state;
+    }
+}
+
+export default todos
