7878921359fae5d1c2f9aae0e07d9f1b9dcb6d15
Spec <dawidjachowicz91@gmail.com>
2020-04-13 21:05:11 +0200

    Improve the prototype of Map component - Move Canvas logic to Map component - Improve logic to read mouse position on canvas - Add prototype of keyboard handling - Change GameWindow structure to fix canvas size calculation

--- src/client/src/components/GameWindow/GameWindow.jsx
+++ src/client/src/components/GameWindow/GameWindow.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux'
 
-import Canvas from "../../Canvas";
-
 import PlayerStatus from "./PlayerStatus";
 import GameMenu from "./GameMenu";
 import InteractionMenu from "./InteractionMenu";
@@ -12,62 +10,28 @@ import Map from "./Map";
 
 import styles from "./styles";
 
-let map = [
-  ['#', '#', ' ', '#'],
-  ['#', '#', ' ', '#'],
-  ['#', '.', '|', '#'],
-  ['#', ' ', '@', '#'],
-  ['#', '#', '#', '#'],
-]
-
-const DisplayOptions = {
-  fontSize: 30,
-  backgroundColor: '#000000', //Black
-  foregroundColor: '#ffffff' //White
-}
-
-const CanvasConfiguration = {
-  width: Math.floor(window.innerWidth / 2),
-  height: Math.floor(window.innerHeight / (2 * DisplayOptions.fontSize)) * DisplayOptions.fontSize
-}
-
 let GameWindow = () => {
-  const canvasRef = React.useRef(null);
-  const [canvas, setCanvas] = useState(null);
-
-  useEffect(() => {
-    setCanvas(new Canvas(canvasRef.current.getContext('2d'), DisplayOptions));
-  }, []);
-
-  useEffect(() => {
-    if (canvas) {
-      canvas.drawCanvas();
-      canvas.draw(map);
-    };
-  }, [canvas]);
+  const [menuText, setMenuText] = useState("");
 
   function handleCanvasClick(e) {
-    const newLocation = { x: e.clientX, y: e.clientY }
-    canvas.mousePosition(newLocation);
+    setMenuText(e);
   }
 
   return (
-    <div className={styles.column}>
-      <div className={styles.row}>
-        <Map
-          canvasRef={canvasRef}
-          canvasConfiguration={CanvasConfiguration}
-          handleCanvasClick={handleCanvasClick}
-        />
-        <PlayerStatus />
-        <GameMenu />
-      </div>
-      <div className={`${styles.row} ${styles.flex1}`}>
-        <div className={`${styles.column} ${styles.flex1}`}>
-          <InteractionMenu />
-          <ActivityLog />
+    <div className={styles.full_height}>
+      <Map handleCanvasClick={handleCanvasClick} />
+      <div className={styles.column}>
+        <div className={styles.row}>
+          <PlayerStatus />
+          <GameMenu />
         </div>
-        <ContextMenu />
+        <div className={`${styles.row} ${styles.flex1}`}>
+          <div className={`${styles.column} ${styles.flex1}`}>
+            <InteractionMenu />
+            <ActivityLog />
+          </div>
+          <ContextMenu text={menuText} />
+        </div>
       </div>
     </div>
   );
