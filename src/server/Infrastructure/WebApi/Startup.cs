5f1fbdc2a4da9938bea9042a398882d42daadf0c
Spec <dawidjachowicz91@gmail.com>
2020-04-14 19:24:50 +0200

    Add test endpoint to return generated map - Set CORS policy to AllowAnyOrigin - Generate and return serialized Map

--- src/server/Infrastructure/WebApi/Startup.cs
+++ src/server/Infrastructure/WebApi/Startup.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Odysseus.DomainModel.MapGenerator;
+using Odysseus.DomainServices.MapGenerator;
+using Odysseus.Framework.Randomizer;
+using System.Text.Json;
 
 namespace WebApi
 {
@@ -16,6 +20,7 @@ namespace WebApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,11 +33,19 @@ namespace WebApi
 
             app.UseRouting();
 
+            app.UseCors(policy => policy.AllowAnyOrigin());
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    var config = new AdjacentRoomsGeneratorConfiguration(10, 3, new ConstraintRange<int>(5, 10), new ConstraintRange<int>(5, 10));
+                    var generator = new ConnectedRoomsDungeonGenerator(new AdjacentRoomsGenerator(config), new CorridorsGenerator(0.4, 2));
+
+                    await context.Response.WriteAsync(JsonSerializer.Serialize(generator.Generate(), new JsonSerializerOptions
+                    {
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                    }));
                 });
             });
         }
