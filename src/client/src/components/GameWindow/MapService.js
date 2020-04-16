2c5b8b2d2265d8be9e36708bd54f822980f53189
Spec <dawid.spec.jachowicz@gmail.com>
2020-04-16 16:21:17 +0200

    Extend and refactor MapService - Rename the service - Add isZero and relativeToScreenCenter functions - Improve logic of contains function

--- src/client/src/components/GameWindow/MapService.js
+++ src/client/src/components/GameWindow/MapService.js
@@ -1,9 +1,17 @@
-export default class Map {
+export default class MapService {
     static contains(tiles, otherTile) {
-        return tiles.some(tile => tile.x == otherTile.x && tile.y == otherTile.y);
+        return tiles.some(tile => tile.x === otherTile.x && tile.y === otherTile.y);
     }
 
     static offset(tile, offset) {
         return { x: tile.x + offset.x, y: tile.y + offset.y };
+    }
+
+    static isZero(offset) {
+        return offset.x === 0 && offset.y === 0;
+    }
+
+    static relativeToScreenCenter(tile, screenCenter) {
+        return { x: tile.x - screenCenter.x, y: tile.y - screenCenter.y }
     }
 }
