027525262cc3cdda447e5fed5e77d9c311e53bb4
Spec <dawidjachowicz91@gmail.com>
2020-04-18 14:21:14 +0200

    Add RayCasting class which performes Ray Casting algorithm - Draws the image on the provided Canvas - Contains functions to manipulate Player

---
+++ src/client/src/components/GameWindow/RayCasting.js
@@ -0,0 +1,92 @@
+import Player from "./Player";
+import DigitalDifferentialAnalyzer from "./DigitalDifferentialAnalyzer";
+
+export default class RayCasting {
+    constructor(canvasRef, map, options) {
+        this.canvasRef = canvasRef;
+        this.context = canvasRef.current.getContext('2d');
+        this.screen = options.screen
+        this.map = map
+        this.moveSpeed = options.moveSpeed
+        this.rotationSpeed = options.rotationSpeed
+        this.player = new Player(options.playerPosition)
+    }
+
+    forward() {
+        this.move(this.moveSpeed)
+    }
+
+    backwards() {
+        this.move(-this.moveSpeed)
+    }
+
+    rotateRight() {
+        this.player.rotate(-this.rotationSpeed)
+    }
+
+    rotateLeft() {
+        this.player.rotate(this.rotationSpeed)
+    }
+
+    move(moveSpeed) {
+        if (this.map[Math.floor(this.player.nextStepX(moveSpeed))][Math.floor(this.player.position.y)] == false)
+            this.player.moveX(moveSpeed)
+
+        if (this.map[Math.floor(this.player.position.x)][Math.floor(this.player.nextStepY(moveSpeed))] == false)
+            this.player.moveY(moveSpeed)
+    }
+
+    drawView() {
+        this.drawBackground()
+
+        for (var verticalScreenStripe = 0; verticalScreenStripe < this.screen.width; verticalScreenStripe++) {
+            var rayDirection = this.rayDirection(verticalScreenStripe)
+
+            var dda = DigitalDifferentialAnalyzer.analyze(this.player.position, rayDirection, (tile) => this.map[tile.x][tile.y] > 0)
+            var perpenducilarWallDistance = this.perpenducilarWallDistance(dda, rayDirection)
+
+            var lineToDraw = this.lineToDraw(perpenducilarWallDistance)
+            this.drawLine(verticalScreenStripe, lineToDraw, dda.wallSide)
+        }
+    }
+
+    drawBackground() {
+        this.context.fillStyle = "black";
+        this.context.fillRect(0, 0, this.screen.width, this.screen.height);
+    }
+
+    rayDirection(verticalScreenStripe) {
+        var cameraPlaneX = 2 * verticalScreenStripe / this.screen.width - 1;
+        return {
+            x: this.player.direction.x + this.player.cameraPlane.x * cameraPlaneX,
+            y: this.player.direction.y + this.player.cameraPlane.y * cameraPlaneX
+        }
+    }
+
+    perpenducilarWallDistance(dda, rayDir) {
+        return dda.wallSide == 0
+            ? (dda.tile.x - this.player.position.x + (1 - dda.step.x) / 2) / rayDir.x
+            : (dda.tile.y - this.player.position.y + (1 - dda.step.y) / 2) / rayDir.y
+    }
+
+    lineToDraw(perpenducilarWallDistance) {
+        var lineHeight = Math.floor(this.screen.height / perpenducilarWallDistance);
+
+        var start = -lineHeight / 2 + this.screen.height / 2;
+        if (start < 0) start = 0;
+
+        var end = lineHeight / 2 + this.screen.height / 2;
+        if (end >= this.screen.height) end = this.screen.height - 1;
+
+        return { start, end }
+    }
+
+    drawLine(verticalScreenStripe, lineToDraw, side) {
+        var color = side == 1 ? "darkgrey" : "grey"
+        this.context.beginPath();
+        this.context.moveTo(verticalScreenStripe, lineToDraw.start);
+        this.context.lineTo(verticalScreenStripe, lineToDraw.end);
+        this.context.strokeStyle = color;
+        this.context.stroke();
+    }
+}
