eec0050859cc53d77a96900fb2d36d3dc247dc20
Spec <dawidjachowicz91@gmail.com>
2020-04-18 14:46:05 +0200

    Add RayCastingView to GameWindow - View button switches between Tiles map and RayCasting - Add test map and configuration for RayCastingView

--- src/client/src/components/GameWindow/GameWindow.jsx
+++ src/client/src/components/GameWindow/GameWindow.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux'
 
 import PlayerStatus from "./PlayerStatus";
@@ -7,53 +7,98 @@ import InteractionMenu from "./InteractionMenu";
 import ActivityLog from "./ActivityLog";
 import ContextMenu from "./ContextMenu";
 import Map from "./Map";
+import RayCastingView from "./RayCastingView";
 
 import styles from "./styles";
 
+const testRayCastingMap =
+    [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ];
+
+const PlayerConfiguration = {
+    playerPosition: { x: 17, y: 14 },
+    rotationSpeed: 0.1,
+    moveSpeed: 0.2
+}
+
 const CanvasConfiguration = {
-  width: Math.floor(window.innerWidth / 2.5),
-  height: window.innerHeight
+    width: Math.floor(window.innerWidth / 2.5),
+    height: window.innerHeight
 }
 
 let GameWindow = () => {
-  const [hoveredTile, setHoveredTile] = useState(null);
+    const [hoveredTile, setHoveredTile] = useState(null);
+    const [isRayCastingOpened, setIsRayCastingOpened] = useState(false);
 
-  function handleCanvasClick(e) {
-    setHoveredTile(`${e.x}, ${e.y}`);
-  }
+    function handleCanvasClick(e) {
+        setHoveredTile(`${e.x}, ${e.y}`);
+    }
 
-  return (
-    <div className={styles.full_height}>
-      <Map
-        canvasConfiguration={CanvasConfiguration}
-        handleCanvasClick={handleCanvasClick} />
-      <div className={styles.column}>
-        <div className={styles.row}>
-          <PlayerStatus hoveredTile={hoveredTile} />
-          <GameMenu />
+    function handleChangeView(e) {
+        setIsRayCastingOpened(!isRayCastingOpened)
+    }
+
+    return (
+        <div className={styles.full_height}>
+            {isRayCastingOpened
+                ? <RayCastingView
+                    canvasConfiguration={CanvasConfiguration}
+                    playerConfiguration={PlayerConfiguration}
+                    map={testRayCastingMap} />
+                : <Map
+                    canvasConfiguration={CanvasConfiguration}
+                    handleCanvasClick={handleCanvasClick} />
+            }
+            <div className={styles.column}>
+                <div className={styles.row}>
+                    <PlayerStatus hoveredTile={hoveredTile} />
+                    <GameMenu handleChangeView={handleChangeView} />
+                </div>
+                <div className={`${styles.row} ${styles.flex1}`}>
+                    <div className={`${styles.column} ${styles.flex1}`}>
+                        <InteractionMenu />
+                        <ActivityLog />
+                    </div>
+                    <ContextMenu />
+                </div>
+            </div>
         </div>
-        <div className={`${styles.row} ${styles.flex1}`}>
-          <div className={`${styles.column} ${styles.flex1}`}>
-            <InteractionMenu />
-            <ActivityLog />
-          </div>
-          <ContextMenu />
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 const mapStateToProps = state => {
-  return {
-    // todos: getVisibleTodos(state.todos, state.visibilityFilter)
-  }
+    return {
+        // todos: getVisibleTodos(state.todos, state.visibilityFilter)
+    }
 }
 
 const mapDispatchToProps = dispatch => {
-  return {
-    // onTodoClick: id => dispatch(toggleTodo(id))
-  }
+    return {
+        // onTodoClick: id => dispatch(toggleTodo(id))
+    }
 }
 
 GameWindow = connect(mapStateToProps, mapDispatchToProps)(GameWindow)
