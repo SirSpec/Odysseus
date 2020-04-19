463741bd9bbf8be935b70e324e05228456341fad
Spec <dawidjachowicz91@gmail.com>
2020-04-19 09:20:34 +0200

    Add test MapHub which sends Generated Map

---
+++ src/server/Infrastructure/WebApi/Hubs/MapHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Odysseus.DomainModel.MapGenerator;
+using Odysseus.DomainServices.MapGenerator;
+using Odysseus.Framework.Randomizer;
+using System.Threading.Tasks;
+
+namespace Odysseus.Infrastructure.WebApi.Hubs
+{
+    public class MapHub : Hub
+    {
+        public async Task SendMap()
+        {
+            var config = new AdjacentRoomsGeneratorConfiguration(10, 3, new ConstraintRange<int>(5, 10), new ConstraintRange<int>(5, 10));
+            var generator = new ConnectedRoomsDungeonGenerator(new AdjacentRoomsGenerator(config), new CorridorsGenerator(0.4, 2));
+
+            await Clients.All.SendAsync("ReceiveMap", generator.Generate());
+        }
+    }
+}
