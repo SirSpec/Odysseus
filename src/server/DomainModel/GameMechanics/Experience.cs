d06371faad9270f4ac7d5d1b2f0ea7c76cd3dd03
Spec <dawidjachowicz91@gmail.com>
2020-04-21 22:36:22 +0200

    Add a prototype of Experience

---
+++ src/server/DomainModel/GameMechanics/Experience.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Odysseus.DomainModel.GameMechanics
+{
+    public class Experience
+    {
+        public event EventHandler<int>? LeveledUp;
+
+        public int Level { get; private set; }
+        public int Points { get; private set; }
+
+        public Experience(int points)
+        {
+            if (points < 0)
+                throw new ArgumentException($"{nameof(points)}:{points} cannot be less than 0.");
+
+            (Points, Level) = (points, CalculateLevel(points));
+        }
+
+        public void Gain(int experience)
+        {
+            if (experience > 0)
+            {
+                Points += experience;
+
+                var level = CalculateLevel(Points);
+                if (level > Level) LeveledUp?.Invoke(this, level);
+
+                Level = level;
+            }
+            else throw new ArgumentException($"{nameof(experience)}:{experience} cannot be less than 0.");
+        }
+
+        private int CalculateLevel(int experience)
+        {
+            return 1;
+        }
+    }
+}
