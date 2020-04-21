7534126e66583e5f11683ecf4b8025e127aa8c17
Spec <dawid.spec.jachowicz@gmail.com>
2020-04-21 22:40:08 +0200

    Add prototypes of Item, Armor, Weapon and Potion

---
+++ src/server/DomainModel/GameMechanics/Weapon.cs
@@ -0,0 +1,13 @@
+﻿namespace Odysseus.DomainModel.GameMechanics
+{
+    public class Weapon : Item
+    {
+        public Damage Damage { get; }
+
+        public Weapon(string name, int weight, Requirements requirements, Damage damage)
+            : base(name, weight, requirements)
+        {
+            Damage = damage;
+        }
+    }
+}
