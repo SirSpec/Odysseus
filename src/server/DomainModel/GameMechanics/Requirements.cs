2a131eeaa2596b8c23a8f0b56957105a8cf706cd
Spec <dawid.spec.jachowicz@gmail.com>
2020-04-21 22:36:56 +0200

    Add a prototype of Requirements

---
+++ src/server/DomainModel/GameMechanics/Requirements.cs
@@ -0,0 +1,17 @@
+﻿namespace Odysseus.DomainModel.GameMechanics
+{
+    public readonly struct Requirements
+    {
+        public Experience Experience { get; }
+        public Attributes Attributes { get; }
+
+        public Requirements(Experience experience, Attributes attributes) =>
+            (Experience, Attributes) = (experience, attributes);
+
+        public bool AreSufficient(Experience experience, Attributes attributes) =>
+            Experience.Level >= experience.Level &&
+            Attributes.Strength >= attributes.Strength &&
+            Attributes.Dexterity >= attributes.Dexterity &&
+            Attributes.Inteligence >= attributes.Inteligence;
+    }
+}
