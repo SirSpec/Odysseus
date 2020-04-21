925b8b3024e7203cc6459338290c7ba6bfaa8126
Spec <dawid.spec.jachowicz@gmail.com>
2020-04-21 22:32:36 +0200

    Add prototypes of Mana and Health

---
+++ src/server/DomainModel/GameMechanics/Health.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Odysseus.DomainModel.GameMechanics
+{
+    public readonly struct Health
+    {
+        public int Current { get; }
+        public int Total { get; }
+        public bool IsDead => Current == 0;
+
+        public Health(int current, int total)
+        {
+            if (current < 0 || total < 0 || current > total)
+                throw new ArgumentException($"{nameof(current)}:{current} or {nameof(total)}:{total} are invalid.");
+
+            (Current, Total) = (current, total);
+        }
+
+        public Health Increase(int value)
+        {
+            var newValue = Current + value >= Total
+                ? Total
+                : Current + value;
+
+            return new Health(newValue, Total);
+        }
+
+        public Health Decrease(int value)
+        {
+            var newValue = Current - value <= 0
+                ? 0
+                : Current - value;
+
+            return new Health(newValue, Total);
+        }
+    }
+}
---
+++ src/server/DomainModel/GameMechanics/Mana.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Odysseus.DomainModel.GameMechanics
+{
+    public readonly struct Mana
+    {
+        public int Current { get; }
+        public int Total { get; }
+
+        public Mana(int current, int total)
+        {
+            if (current < 0 || total < 0 || current > total)
+                throw new ArgumentException($"{nameof(current)}:{current} or {nameof(total)}:{total} are invalid.");
+
+            (Current, Total) = (current, total);
+        }
+
+        public Mana Increase(int value)
+        {
+            var newValue = Current + value >= Total
+                ? Total
+                : Current + value;
+
+            return new Mana(newValue, Total);
+        }
+
+        public Mana Decrease(int value)
+        {
+            var newValue = Current - value <= 0
+                ? 0
+                : Current - value;
+
+            return new Mana(newValue, Total);
+        }
+    }
+}
