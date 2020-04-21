05aff918dc552f7c8887589f7fd24049fd7770d1
Spec <dawid.spec.jachowicz@gmail.com>
2020-04-21 22:39:38 +0200

    Add prototypes of Spell and SpellBook

---
+++ src/server/DomainModel/GameMechanics/Spell.cs
@@ -0,0 +1,12 @@
+﻿namespace Odysseus.DomainModel.GameMechanics
+{
+    public readonly struct Spell
+    {
+        public string Name { get; }
+        public Effect Effect { get; }
+        public Requirements Requirements { get; }
+
+        public Spell(string name, Effect effect, Requirements requirements) =>
+            (Name, Effect, Requirements) = (name, effect, requirements);
+    }
+}
