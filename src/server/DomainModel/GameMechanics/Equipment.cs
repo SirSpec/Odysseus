3c414248cb44687564beadc9663e8e9e40b7a1b0
Spec <dawidjachowicz91@gmail.com>
2020-04-21 22:41:21 +0200

    Add a prototype of Equipment

---
+++ src/server/DomainModel/GameMechanics/Equipment.cs
@@ -0,0 +1,17 @@
+﻿namespace Odysseus.DomainModel.GameMechanics
+{
+    public struct Equipment
+    {
+        public Armor? BodyArmour { get; }
+        public Armor? Boots { get; }
+        public Armor? Gloves { get; }
+        public Armor? Helmet { get; }
+
+        public Item? MainHand { get; }
+        public Item? OffHand { get; }
+
+        public void Equip(Item item)
+        {
+        }
+    }
+}
