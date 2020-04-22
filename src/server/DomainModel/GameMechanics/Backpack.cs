4c69053796d8590a7d2fa5e378ca72dea3bc07e7
Spec <dawid.spec.jachowicz@gmail.com>
2020-04-22 09:12:58 +0200

    Add a prototype of Backpack

---
+++ src/server/DomainModel/GameMechanics/Backpack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Odysseus.DomainModel.GameMechanics
+{
+    public class Backpack
+    {
+        private readonly List<Item> items;
+
+        public int Capacity { get; }
+        public IEnumerable<Item> Items => items;
+        public bool IsFull => Items.Count() == Capacity;
+        public double Weight => Items.Sum(item => item.Weight);
+
+        public Backpack(int capacity) =>
+            (items, Capacity) = (new List<Item>(), capacity);
+
+        public bool Contains(Item item) =>
+            Items.Contains(item);
+
+        public void Put(Item item)
+        {
+            if (!IsFull) items.Add(item);
+            else throw new InvalidOperationException($"{nameof(Backpack)} of {nameof(Capacity)}:{Capacity} is already full.");
+        }
+
+        public void Remove(Item item)
+        {
+            if (items.Contains(item)) items.Remove(item);
+            else throw new InvalidOperationException($"Item {item} does not exists.");
+        }
+    }
+}
