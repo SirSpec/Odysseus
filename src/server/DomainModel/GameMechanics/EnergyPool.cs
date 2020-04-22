9b65989598546bb51cd811750bebc23e24ac3026
Spec <dawidjachowicz91@gmail.com>
2020-04-22 21:02:34 +0200

    Add implementation of EnergyPool and corresponding Unit Tests - Change implementation of Mana and Health to use EnergyPool

---
+++ src/server/DomainModel/GameMechanics/EnergyPool.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Odysseus.DomainModel.GameMechanics
+{
+    public abstract class EnergyPool
+    {
+        public int Current { get; private set; }
+        public int Total { get; }
+
+        public EnergyPool(int total)
+        {
+            if (total < 0)
+                throw new ArgumentException($"{nameof(total)}:{total} cannot be negative.");
+
+            (Current, Total) = (total, total);
+        }
+
+        public EnergyPool(int current, int total)
+        {
+            if (current < 0 || total < 0 || current > total)
+                throw new ArgumentException($"{nameof(current)}:{current} and {nameof(total)}:{total} are invalid.");
+
+            (Current, Total) = (current, total);
+        }
+
+        public void Increase(int value) =>
+            Current = Current + value >= Total
+                ? Total
+                : Current + value;
+
+        public void Decrease(int value) =>
+            Current = Current - value <= 0
+                ? 0
+                : Current - value;
+    }
+}
