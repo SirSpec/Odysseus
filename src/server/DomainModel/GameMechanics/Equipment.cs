0bc023086bcfa8aa4f0e90b6c42618118f56edb7
Spec <dawid.spec.jachowicz@gmail.com>
2020-04-22 09:13:59 +0200

    Add Damage and Armor methods to Equipment

--- src/server/DomainModel/GameMechanics/Equipment.cs
+++ src/server/DomainModel/GameMechanics/Equipment.cs
@@ -1,17 +1,35 @@
 ﻿namespace Odysseus.DomainModel.GameMechanics
 {
-    public struct Equipment
+    public class Equipment
     {
-        public Armor? BodyArmour { get; }
-        public Armor? Boots { get; }
-        public Armor? Gloves { get; }
-        public Armor? Helmet { get; }
+        public Armor? BodyArmour { get; set; }
+        public Armor? Boots { get; set; }
+        public Armor? Gloves { get; set; }
+        public Armor? Helmet { get; set; }
 
-        public Item? MainHand { get; }
-        public Item? OffHand { get; }
+        public Weapon? MainHand { get; set; }
+        public Item? OffHand { get; set; }
 
-        public void Equip(Item item)
+        public double Weight =>
+            BodyArmour?.Weight ?? 0.0 +
+            Boots?.Weight ?? 0.0 +
+            Gloves?.Weight ?? 0.0 +
+            Helmet?.Weight ?? 0.0 +
+            MainHand?.Weight ?? 0.0 +
+            OffHand?.Weight ?? 0.0;
+
+        public Damage[] Damage => (MainHand, OffHand) switch
         {
-        }
+            (Weapon mainHand, Weapon offHand) => new[] { mainHand.Damage, offHand.Damage },
+            (Weapon mainHand, _) => new[] { mainHand.Damage },
+            _ => new Damage[] { }
+        };
+
+        public int Armor =>
+            BodyArmour?.Value ?? 0 +
+            Boots?.Value ?? 0 +
+            Gloves?.Value ?? 0 +
+            Helmet?.Value ?? 0 +
+            ((OffHand is Armor offHand) ? offHand.Value : 0);
     }
 }
