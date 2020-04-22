8d9c16249bea9e276a73754e557d3aa1f4d35a6e
Spec <dawid.spec.jachowicz@gmail.com>
2020-04-22 22:07:33 +0200

    Refactor Damage - Add input check and throw Exception if invalid

--- src/server/DomainModel/GameMechanics/Damage.cs
+++ src/server/DomainModel/GameMechanics/Damage.cs
@@ -1,11 +1,18 @@
-﻿namespace Odysseus.DomainModel.GameMechanics
+﻿using System;
+
+namespace Odysseus.DomainModel.GameMechanics
 {
     public readonly struct Damage
     {
         public int Value { get; }
         public DamageType Type { get; }
 
-        public Damage(int value, DamageType type) =>
+        public Damage(int value, DamageType type)
+        {
+            if(value < 0)
+                throw new ArgumentException($"{nameof(value)}:{value} cannot be negative.");
+
             (Value, Type) = (value, type);
+        }
     }
 }
