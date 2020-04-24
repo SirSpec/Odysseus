d885a0f6fce1a6a1aa3823d2663af6d112f7ee36
Spec <dawid.spec.jachowicz@gmail.com>
2020-04-24 22:21:35 +0200

    Add Gold and corresponding Unit Tests

---
+++ src/server/DomainModel/GameMechanics/Gold.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Odysseus.DomainModel.GameMechanics
+{
+    public readonly struct Gold : IEquatable<Gold>
+    {
+        private const int Minimum = 0;
+
+        public int Value { get; }
+
+        public Gold(int value)
+        {
+            if (value < Minimum)
+                throw new ArgumentException($"{nameof(value)}:{value} cannot be less than {Minimum}.");
+
+            Value = value;
+        }
+
+        public bool Equals(Gold other) =>
+            Value == other.Value;
+    }
+}
