91f34bcb66c2d43db651248b054c35f87e2ced00
Spec <dawidjachowicz91@gmail.com>
2020-04-24 21:50:31 +0200

    Add Melee and Ranged Damage Types, refactor Damage, add Unit Tests

--- src/server/DomainModel/GameMechanics/Damage.cs
+++ src/server/DomainModel/GameMechanics/Damage.cs
@@ -4,12 +4,14 @@ namespace Odysseus.DomainModel.GameMechanics
 {
     public readonly struct Damage
     {
+        private const int Minimum = 0;
+
         public int Value { get; }
         public DamageType Type { get; }
 
         public Damage(int value, DamageType type)
         {
-            if(value < 0)
+            if(value < Minimum)
                 throw new ArgumentException($"{nameof(value)}:{value} cannot be negative.");
 
             (Value, Type) = (value, type);
