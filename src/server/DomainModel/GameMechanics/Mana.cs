8cf4125bff6c9f30e9be6d0644aa03873f8c25de
Spec <dawid.spec.jachowicz@gmail.com>
2020-04-24 21:27:42 +0200

    Change Mana and add corresponding Unit Tests

--- src/server/DomainModel/GameMechanics/Mana.cs
+++ src/server/DomainModel/GameMechanics/Mana.cs
@@ -1,12 +1,17 @@
-﻿namespace Odysseus.DomainModel.GameMechanics
+﻿using System;
+
+namespace Odysseus.DomainModel.GameMechanics
 {
-    public readonly struct Mana
+    public readonly struct Mana : IEquatable<Health>
     {
+        private const int ManaPerLevel = 6;
+
         public int Value { get; }
 
-        public Mana(Experience experience, Attributes attributes)
-        {
-            Value = experience.Level + attributes.Inteligence;
-        }
+        public Mana(Level level, CharacterAttribute intelligence) =>
+            Value = ManaPerLevel * level.Value + intelligence.Value;
+
+        public bool Equals(Health other) =>
+            Value == other.Value;
     }
 }
---
+++ test/DomainModel/GameMechanicsTest/ManaTest.cs
@@ -0,0 +1,47 @@
+using Odysseus.DomainModel.GameMechanics;
+using Xunit;
+
+namespace Odysseus.DomainModel.GameMechanicsTest
+{
+    public class ManaTest
+    {
+        [Fact]
+        public void Constructor_NewCharacter_7()
+        {
+            //Arrange
+            var sut = new Mana(new Level(1), new CharacterAttribute(1));
+
+            //Act
+            var result = sut.Value;
+
+            //Assert
+            Assert.Equal(7, result);
+        }
+
+        [Fact]
+        public void Constructor_MaxLevelCharacterWithNoIntelligence_601()
+        {
+            //Arrange
+            var sut = new Mana(new Level(100), new CharacterAttribute(1));
+
+            //Act
+            var result = sut.Value;
+
+            //Assert
+            Assert.Equal(601, result);
+        }
+
+        [Fact]
+        public void Constructor_MaxLevelCharacterWith100Intelligence_700()
+        {
+            //Arrange
+            var sut = new Mana(new Level(100), new CharacterAttribute(100));
+
+            //Act
+            var result = sut.Value;
+
+            //Assert
+            Assert.Equal(700, result);
+        }
+    }
+}
