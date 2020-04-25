78580f9419cd1d6061e50593733b7371b895a165
Spec <dawidjachowicz91@gmail.com>
2020-04-25 17:51:35 +0200

    Refactor GameMechanics project - Refactor prototype implementation - Move implementations into corresponding folders

--- src/server/DomainModel/GameMechanics/Misc/Backpack.cs
+++ src/server/DomainModel/GameMechanics/Misc/Backpack.cs
@@ -6,26 +6,26 @@ namespace Odysseus.DomainModel.GameMechanics
 {
     public class Backpack
     {
-        private readonly IList<Item> items;
+        private readonly IList<IItem> items;
 
         public int Capacity { get; }
-        public IEnumerable<Item> Items => items;
+        public IEnumerable<IItem> Items => items;
         public bool IsFull => Items.Count() == Capacity;
         public double Weight => Items.Sum(item => item.Weight);
 
         public Backpack(int capacity) =>
-            (items, Capacity) = (new List<Item>(), capacity);
+            (items, Capacity) = (new List<IItem>(), capacity);
 
-        public bool Contains(Item item) =>
+        public bool Contains(IItem item) =>
             Items.Contains(item);
 
-        public void Put(Item item)
+        public void Put(IItem item)
         {
             if (!IsFull) items.Add(item);
             else throw new InvalidOperationException($"{nameof(Backpack)} of {nameof(Capacity)}:{Capacity} is already full.");
         }
 
-        public void Remove(Item item)
+        public void Remove(IItem item)
         {
             if (items.Contains(item)) items.Remove(item);
             else throw new InvalidOperationException($"Item {item} does not exists.");
