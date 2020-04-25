576ecd9a7d11108b1a75f98802565d2fb76b08fb
Spec <dawid.spec.jachowicz@gmail.com>
2020-04-25 15:23:12 +0200

    Change Health and Mana - Implement ISpecification interface - Make compatible with Statistics class

--- test/DomainModel/GameMechanicsTest/ManaTest.cs
+++ test/DomainModel/GameMechanicsTest/ManaTest.cs
@@ -6,10 +6,23 @@ namespace Odysseus.DomainModel.GameMechanicsTest
     public class ManaTest
     {
         [Fact]
+        public void Constructor_Default_7()
+        {
+            //Arrange
+            var sut = new Mana();
+
+            //Act
+            var result = sut.Value;
+
+            //Assert
+            Assert.Equal(7, result);
+        }
+
+        [Fact]
         public void Constructor_NewCharacter_7()
         {
             //Arrange
-            var sut = new Mana(new Level(1), new CharacterAttribute(1));
+            var sut = new Mana(new Level(1), new Intelligence(1));
 
             //Act
             var result = sut.Value;
@@ -22,7 +35,7 @@ namespace Odysseus.DomainModel.GameMechanicsTest
         public void Constructor_MaxLevelCharacterWithNoIntelligence_601()
         {
             //Arrange
-            var sut = new Mana(new Level(100), new CharacterAttribute(1));
+            var sut = new Mana(new Level(100), new Intelligence(1));
 
             //Act
             var result = sut.Value;
@@ -35,7 +48,7 @@ namespace Odysseus.DomainModel.GameMechanicsTest
         public void Constructor_MaxLevelCharacterWith100Intelligence_700()
         {
             //Arrange
-            var sut = new Mana(new Level(100), new CharacterAttribute(100));
+            var sut = new Mana(new Level(100), new Intelligence(100));
 
             //Act
             var result = sut.Value;
