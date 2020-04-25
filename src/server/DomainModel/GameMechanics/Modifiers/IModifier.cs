2a55dd20e492c7e097b09eeac823c85f44f5f69d
Spec <dawidjachowicz91@gmail.com>
2020-04-25 15:49:43 +0200

    Add IModifier

---
+++ src/server/DomainModel/GameMechanics/Modifiers/IModifier.cs
@@ -0,0 +1,7 @@
+﻿namespace Odysseus.DomainModel.GameMechanics
+{
+    public interface IModifier<out TType> where TType : class, IStatistic, new()
+    {
+        IStatistic Modify(IStatistic statistic);
+    }
+}
