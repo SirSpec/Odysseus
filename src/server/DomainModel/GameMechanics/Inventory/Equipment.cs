c9ac3bfb84fa329d17dac87a0f67d0688d5b47ba
Spec <dawidjachowicz91@gmail.com>
2020-04-26 21:46:20 +0200

    Change Equipment to use Slot and Equiped Event

---
+++ src/server/DomainModel/GameMechanics/Inventory/Equipment.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Odysseus.DomainModel.GameMechanics
+{
+    public class Equipment
+    {
+        public event EventHandler<EquipedEventArgs>? Equiped;
+
+        //TODO: turn into list
+        private readonly Slot bodyArmour, boots, gloves, helmet, mainHand, offHand;
+        
+        public IItem BodyArmour => bodyArmour.Item;
+        public IItem Boots => boots.Item;
+        public IItem Gloves => gloves.Item;
+        public IItem Helmet => helmet.Item;
+        public IItem MainHand => mainHand.Item;
+        public IItem OffHand => offHand.Item;
+
+        public Equipment()
+        {
+            bodyArmour = new Slot();
+            boots = new Slot();
+            gloves = new Slot();
+            helmet = new Slot();
+            mainHand = new Slot();
+            offHand = new Slot();
+        }
+        //turn into list
+        public void Equip(IItem item)
+        {
+            if(item is BodyArmor)
+            {
+                if(bodyArmour.IsEmpty)
+                {
+                    var oldItem = bodyArmour.Item;
+                    bodyArmour.Item = item;
+                    Equiped?.Invoke(this, new EquipedEventArgs());
+                } else
+                {
+                    bodyArmour.Item = item;
+                    Equiped?.Invoke(this, new EquipedEventArgs());
+                }
+            }
+        }
+
+        public double Weight =>
+            BodyArmour?.Weight ?? 0.0 +
+            Boots?.Weight ?? 0.0 +
+            Gloves?.Weight ?? 0.0 +
+            Helmet?.Weight ?? 0.0 +
+            MainHand?.Weight ?? 0.0 +
+            OffHand?.Weight ?? 0.0;
+    }
+}
