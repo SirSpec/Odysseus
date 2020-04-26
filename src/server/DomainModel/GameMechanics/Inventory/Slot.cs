2ad4e7d1ecaf322c54a0da5a09b510b88521bb66
Spec <dawidjachowicz91@gmail.com>
2020-04-26 21:44:55 +0200

    Add Slot implementation

---
+++ src/server/DomainModel/GameMechanics/Inventory/Slot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Odysseus.DomainModel.GameMechanics
+{
+    public class Slot
+    {
+        private IItem? item;
+        public bool IsEmpty => item is null;
+        public IItem Item
+        {
+            get => item ?? throw new InvalidOperationException();
+            set => item = value;
+        }
+    }
+}
