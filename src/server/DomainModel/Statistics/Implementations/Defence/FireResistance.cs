075cae950dcb319903814f7c470847fe228b2c8b
Spec <dawid.spec.jachowicz@gmail.com>
2020-04-29 22:30:20 +0200

    Add new prototypes of Implementations of Statistics

---
+++ src/server/DomainModel/Statistics/Implementations/Defence/FireResistance.cs
@@ -0,0 +1,18 @@
+﻿using Odysseus.DomainModel.Statistics.Base;
+
+namespace Odysseus.DomainModel.Statistics.Implementations.Defence
+{
+    public class FireResistance : Statistic, IPrimaryStatistic, ILevelable
+    {
+        private const int Minimum = 0;
+        private const int Maximum = 70;
+        public int BaseValue { get; set; } = 0;
+
+        public int Value => BaseValue >= Maximum ? Maximum : BaseValue;
+
+        public void LevelUp()
+        {
+            BaseValue++;
+        }
+    }
+}
---
+++ src/server/DomainModel/Statistics/Implementations/Offence/MeleeDamage.cs
@@ -0,0 +1,19 @@
+﻿using Odysseus.DomainModel.Statistics.Base;
+using Odysseus.DomainModel.Statistics.Implementations.Attributes;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Odysseus.DomainModel.GameMechanics.Statistics.Offence
+{
+    public class MeleeDamage : Statistic, IDerivedStatistic
+    {
+        public int Value { get; }
+
+        public int DeriveValue(IEnumerable<IPrimaryStatistic> statistics)
+        {
+            var intelligence = statistics.Single(s => s is Strength);
+            var baseValue = (Value + intelligence.Value);
+            return baseValue + Enhancements.Sum(en => en.Enhance(baseValue));
+        }
+    }
+}
