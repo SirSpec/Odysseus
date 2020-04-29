0bfa6e9d781fbae53cb0f250ea21a5c90bd8c9a2
Spec <dawidjachowicz91@gmail.com>
2020-04-29 22:20:00 +0200

    Add a new implementation of StatisticsSet - Change according to the new abstractions - Add corresponding Unit Tests

---
+++ src/server/DomainModel/Statistics/StatisticsSet.cs
@@ -0,0 +1,52 @@
+﻿using Odysseus.DomainModel.Statistics.Base;
+using Odysseus.DomainModel.Statistics.Enhancements;
+using Odysseus.Framework.Assembler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Odysseus.DomainModel.Statistics
+{
+    public class StatisticsSet
+    {
+        private readonly IEnumerable<IStatistic> statistics;
+
+        public StatisticsSet() =>
+            statistics = Assembler.CreateImplementationsOf<IStatistic>().ToList();
+
+        public IEnumerable<TStatistic> GetStatistics<TStatistic>() where TStatistic : IStatistic =>
+            FindStatistics(typeof(TStatistic))
+                .Select(statistic => (TStatistic)statistic);
+
+        public int GetPrimaryStatistic<TStatistic>() where TStatistic : IPrimaryStatistic =>
+            GetStatistics<TStatistic>()
+                .Single()
+                .Value;
+
+        public int GetDerivedStatistic<TStatistic>() where TStatistic : IDerivedStatistic =>
+            GetStatistics<TStatistic>()
+                .Single()
+                .DeriveValue(GetStatistics<IPrimaryStatistic>());
+
+        public void Apply(params IEnhancement<IStatistic>[] enhancements)
+        {
+            foreach (var enhancement in enhancements)
+            {
+                var statisticType = enhancement.GetType().GenericTypeArguments.Single();
+                FindStatistics(statisticType).Single().AddEnhancement(enhancement);
+            }
+        }
+
+        public void Remove(params IEnhancement<IStatistic>[] enhancements)
+        {
+            foreach (var enhancement in enhancements)
+            {
+                var statisticType = enhancement.GetType().GenericTypeArguments.Single();
+                FindStatistics(statisticType).Single().RemoveEnhancement(enhancement);
+            }
+        }
+
+        private IEnumerable<IStatistic> FindStatistics(Type type) =>
+            statistics.Where(statistic => type.IsAssignableFrom(statistic.GetType()));
+    }
+}
---
+++ test/DomainModel/StatisticsTest/StatisticsSetTest.cs
@@ -0,0 +1,166 @@
+using System;
+using Odysseus.DomainModel.Statistics;
+using Odysseus.DomainModel.Statistics.Base;
+using Odysseus.DomainModel.Statistics.Enhancements;
+using Odysseus.DomainModel.Statistics.Implementations.Attributes;
+using Odysseus.DomainModel.Statistics.Implementations.Main;
+using Xunit;
+
+namespace Odysseus.DomainModel.StatisticsTest
+{
+    public class StatisticsSetTest
+    {
+        [Fact]
+        public void GetStatistics_IStatistic_AllStatistics()
+        {
+            //Arrange
+            var sut = new StatisticsSet();
+
+            //Act
+            var results = sut.GetStatistics<IStatistic>();
+
+            //Assert
+            Assert.NotEmpty(results);
+        }
+
+        [Fact]
+        public void GetStatistics_IPrimaryStatistic_AllPrimaryStatistics()
+        {
+            //Arrange
+            var sut = new StatisticsSet();
+
+            //Act
+            var results = sut.GetStatistics<IPrimaryStatistic>();
+
+            //Assert
+            Assert.NotEmpty(results);
+        }
+
+        [Fact]
+        public void GetStatistics_IPrimaryStatistic_AllDerivedStatistics()
+        {
+            //Arrange
+            var sut = new StatisticsSet();
+
+            //Act
+            var results = sut.GetStatistics<IDerivedStatistic>();
+
+            //Assert
+            Assert.NotEmpty(results);
+        }
+
+        [Fact]
+        public void GetPrimaryStatistic_IPrimaryStatistic_ThrowsInvalidOperationException()
+        {
+            //Arrange
+            var sut = new StatisticsSet();
+            
+            //Act
+            Func<object> results = () => sut.GetPrimaryStatistic<IPrimaryStatistic>();
+
+            //Assert
+            Assert.Throws<InvalidOperationException>(results);
+        }
+
+        [Fact]
+        public void GetPrimaryStatistic_Strength_StrengthValue()
+        {
+            //Arrange
+            var sut = new StatisticsSet();
+
+            //Act
+            var results = sut.GetPrimaryStatistic<Strength>();
+
+            //Assert
+            Assert.Equal(1, results);
+        }
+
+        [Fact]
+        public void GetDerivedStatistic_IDerivedStatistic_ThrowsInvalidOperationException()
+        {
+            //Arrange
+            var sut = new StatisticsSet();
+
+            //Act
+            Func<object> results = () => sut.GetDerivedStatistic<IDerivedStatistic>();
+
+            //Assert
+            Assert.Throws<InvalidOperationException>(results);
+        }
+
+        [Fact]
+        public void GetDerivedStatistic_Health_HealthValue()
+        {
+            //Arrange
+            var sut = new StatisticsSet();
+
+            //Act
+            var results = sut.GetDerivedStatistic<Health>();
+
+            //Assert
+            Assert.Equal(13, results);
+        }
+
+        [Fact]
+        public void Apply_FlatHealthModifier_NewHealthValue()
+        {
+            //Arrange
+            var statistics = new StatisticsSet();
+            var sut = new FlatEnhancement<Health>(10);
+            statistics.Apply(sut);
+
+            //Act
+            var result = statistics.GetDerivedStatistic<Health>();
+
+            //Assert
+            Assert.Equal(23, result);
+        }
+
+        [Fact]
+        public void Apply_MultipleHealthModifier_NewHealthValue()
+        {
+            //Arrange
+            var statistics = new StatisticsSet();
+            var sut = new IEnhancement<IStatistic>[] { new FlatEnhancement<Health>(10), new PercentageEnhancement<Health>(20) };
+            statistics.Apply(sut);
+
+            //Act
+            var result = statistics.GetDerivedStatistic<Health>();
+
+            //Assert
+            Assert.Equal(25, result);
+        }
+
+        [Fact]
+        public void Remove_FlatHealthModifier_DefaultValue()
+        {
+            //Arrange
+            var statistics = new StatisticsSet();
+            var sut = new FlatEnhancement<Health>(10);
+            statistics.Apply(sut);
+
+            //Act
+            statistics.Remove(sut);
+            var result = statistics.GetDerivedStatistic<Health>();
+
+            //Assert
+            Assert.Equal(13, result);
+        }
+
+        [Fact]
+        public void Remove_MultipleHealthModifier_DefaultValue()
+        {
+            //Arrange
+            var statistics = new StatisticsSet();
+            var sut = new IEnhancement<IStatistic>[] { new FlatEnhancement<Health>(10), new PercentageEnhancement<Health>(20) };
+            statistics.Apply(sut);
+
+            //Act
+            statistics.Remove(sut);
+            var result = statistics.GetDerivedStatistic<Health>();
+
+            //Assert
+            Assert.Equal(13, result);
+        }
+    }
+}
