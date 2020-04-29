ca2323f42e3db1bda9c18de87713df19eba638f7
Spec <dawidjachowicz91@gmail.com>
2020-04-29 22:16:38 +0200

    Add new Implementations of Statistics - Change Implementations according to the new abstractions

---
+++ src/server/DomainModel/Statistics/Implementations/Defence/Armor.cs
@@ -0,0 +1,20 @@
+﻿using Odysseus.DomainModel.Statistics.Base;
+using System.Linq;
+
+namespace Odysseus.DomainModel.Statistics.Implementations.Defence
+{
+    public class Armor : Statistic, IPrimaryStatistic, ILevelable
+    {
+        private const int Minimum = 0;
+        public int Value => BaseValue + Enhancements.Sum(e => e.Enhance(BaseValue));
+        public int BaseValue { get; set; }
+
+        public Armor() =>
+            BaseValue = Minimum;
+
+        public void LevelUp()
+        {
+            BaseValue++;
+        }
+    }
+}
---
+++ src/server/DomainModel/Statistics/Implementations/Main/Mana.cs
@@ -0,0 +1,27 @@
+﻿using Odysseus.DomainModel.Statistics;
+using Odysseus.DomainModel.Statistics.Base;
+using Odysseus.DomainModel.Statistics.Implementations.Attributes;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Odysseus.DomainModel.GameMechanics.Statistics.Main
+{
+    public class Mana : Statistic, IDerivedStatistic, ILevelable
+    {
+        private const int Minimum = 1;
+        private const int ManaPerLevel = 6;
+        private int Level { get; set; } = Minimum;
+
+        public void LevelUp()
+        {
+            Level++;
+        }
+
+        public int DeriveValue(IEnumerable<IPrimaryStatistic> statistics)
+        {
+            var intelligence = statistics.Single(s => s is Intelligence);
+            var baseValue = (ManaPerLevel * Level + intelligence.Value);
+            return baseValue + Enhancements.Sum(en => en.Enhance(baseValue));
+        }
+    }
+}
