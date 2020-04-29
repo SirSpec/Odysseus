66a0eb7b4c78b0f8eb332627a31d5342cc1a559a
Spec <dawid.spec.jachowicz@gmail.com>
2020-04-29 22:17:53 +0200

    Add a new implementation of modifiers - Rename to Enhancement

---
+++ src/server/DomainModel/Statistics/Enhancements/FlatEnhancement.cs
@@ -0,0 +1,14 @@
+﻿using Odysseus.DomainModel.Statistics.Base;
+
+namespace Odysseus.DomainModel.Statistics.Enhancements
+{
+    public class FlatEnhancement<TStatistic> : IEnhancement<TStatistic> where TStatistic : class, IStatistic
+    {
+        private readonly int value;
+
+        public FlatEnhancement(int value) =>
+            this.value = value;
+
+        public int Enhance(int _) => value;
+    }
+}
