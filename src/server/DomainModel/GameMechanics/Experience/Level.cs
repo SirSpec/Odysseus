631929eb56d2e5444ed9dadf08f60a1e47a2c7e9
Spec <dawidjachowicz91@gmail.com>
2020-04-30 10:41:38 +0200

    Add IEquatable to Level

--- src/server/DomainModel/GameMechanics/Experience/Level.cs
+++ src/server/DomainModel/GameMechanics/Experience/Level.cs
@@ -2,13 +2,14 @@
 
 namespace Odysseus.DomainModel.GameMechanics.Experience
 {
-    public class Level
+    public class Level : IEquatable<Level>
     {
         public const int Minimum = 1;
         public const int Maximum = 100;
 
         public int Value { get; }
         public bool IsMaximum => Value == Maximum;
+        public static Level One => new Level(1);
 
         public Level(int level)
         {
@@ -17,5 +18,8 @@ namespace Odysseus.DomainModel.GameMechanics.Experience
 
             Value = level;
         }
+
+        public bool Equals(Level other) =>
+            Value == other.Value;
     }
 }
