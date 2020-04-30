75e3656e092cef40fafb8b6a81c2b68b673a9f80
Spec <dawid.spec.jachowicz@gmail.com>
2020-04-30 07:52:21 +0200

    Add Unit Tests for Enhancements

---
+++ test/DomainModel/StatisticsTest/Enhancements/EnhancementTest.cs
@@ -0,0 +1,36 @@
+using Odysseus.DomainModel.Statistics.Base;
+using Odysseus.DomainModel.Statistics.Enhancements;
+using Xunit;
+
+namespace Odysseus.DomainModel.StatisticsTest.Enhancements
+{
+    public class EnhancementTest
+    {
+        [Fact]
+        public void FlatEnhance_One_One()
+        {
+            //Arrange
+            var flatValue = 5;
+            var sut = new FlatEnhancement<IStatistic>(flatValue);
+
+            //Act
+            var results = sut.Enhance(2);
+
+            //Assert
+            Assert.Equal(flatValue, results);
+        }
+
+        [Fact]
+        public void PercentageEnhancement_50_50PercentOfValue()
+        {
+            //Arrange
+            var sut = new PercentageEnhancement<IStatistic>(50);
+
+            //Act
+            var results = sut.Enhance(10);
+
+            //Assert
+            Assert.Equal(5, results);
+        }
+    }
+}
