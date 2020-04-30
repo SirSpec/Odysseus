00ad85d65bfc3c3e8ac24f21366569e906f2be2c
Spec <dawid.spec.jachowicz@gmail.com>
2020-04-30 22:18:11 +0200

    Uncomment code in PlayerCharacter implementation

--- src/server/DomainModel/GameMechanics/Characters/PlayerCharacter.cs
+++ src/server/DomainModel/GameMechanics/Characters/PlayerCharacter.cs
@@ -3,6 +3,12 @@ using Odysseus.DomainModel.GameMechanics.Statistics.Implementations.Offence;
 using Odysseus.DomainModel.GameMechanics.Statistics;
 using System;
 using Odysseus.DomainModel.GameMechanics.Inventory;
+using Odysseus.DomainModel.GameMechanics.Statistics.Implementations.Main;
+using System.Linq;
+using Odysseus.DomainModel.GameMechanics.Statistics.Base;
+using Odysseus.DomainModel.GameMechanics.Enhancements;
+using Odysseus.DomainModel.GameMechanics.Statistics.Implementations.Attributes;
+using Odysseus.DomainModel.GameMechanics.Statistics.Implementations.Defence;
 
 namespace Odysseus.DomainModel.GameMechanics
 {
@@ -25,59 +31,54 @@ namespace Odysseus.DomainModel.GameMechanics
             Experience.LeveledUp += OnLeveledUpHandler;
 
             BaseStatistics = new StatisticsSet();
-            //EnergyPool = new EnergyPool(BaseStatistics.GetStatistic<Health>().Value);
+            EnergyPool = new EnergyPool(BaseStatistics.GetDerivedStatistic<Health>());
             var eq = new Equipment();
             eq.Equiped += OnEquiped;
 
-            Inventory = new InventorySet(eq, new Backpack(10), 10);
+            Inventory = new InventorySet(eq, new Backpack(10), new Weight(10));
             SpellBook = new SpellBook();
         }
 
         public int Attack()
         {
-            return 0;//BaseStatistics.GetStatistic<MeleeDamage>();
+            return BaseStatistics.GetDerivedStatistic<MeleeDamage>();
         }
 
-        public void TakeDamage(MeleeDamage damage)
+        public void TakeDamage(Damage damage)
         {
-            //var d = damage switch
-            //{
-            //    Damage dmg when dmg is MeleeDamage || dmg is RangedDamage => dmg.Value - BaseStatistics.GetStatistic<Armor>().Value,
-            //    FireDamage dmg => dmg.Value - BaseStatistics.GetStatistic<FireResistance>().Value,
-            //    IceDamage dmg => dmg.Value - BaseStatistics.GetStatistic<IceDamage>().Value,
-            //    LightningDamage dmg => dmg.Value - BaseStatistics.GetStatistic<LightningDamage>().Value,
-            //    _ => throw new InvalidCastException(),
-            //};
+            var d = damage switch
+            {
+                Damage dmg when dmg is MeleeDamage || dmg is RangedDamage => dmg.Value - BaseStatistics.GetPrimaryStatistic<Armor>(),
+                FireDamage dmg => dmg.Value - BaseStatistics.GetPrimaryStatistic<FireResistance>(),
+                IceDamage dmg => dmg.Value - BaseStatistics.GetPrimaryStatistic<IceResistance>(),
+                LightningDamage dmg => dmg.Value - BaseStatistics.GetPrimaryStatistic<LightningResistance>(),
+                _ => throw new InvalidCastException(),
+            };
 
-            //EnergyPool.Decrease(d);
+            EnergyPool.Decrease(d);
         }
 
         public void OnEquiped(object _, EquipedEventArgs args)
         {
-            //var oldMods = args.OldItem?.Modifiers ?? Enumerable.Empty<IModifier<Statistic>>();
-            //foreach (var mod in oldMods)
-            //{
-            //    BaseStatistics.Remove(mod);
-            //}
+            var oldMods = args.OldItem?.Enhancements ?? Enumerable.Empty<IEnhancement<IStatistic>>();
+            foreach (var mod in oldMods)
+            {
+                BaseStatistics.Remove(mod);
+            }
 
-            //var newMods = args.NewItem?.Modifiers ?? Enumerable.Empty<IModifier<Statistic>>();
-            //foreach (var mod in newMods)
-            //{
-            //    BaseStatistics.Apply(mod);
-            //}
+            var newMods = args.NewItem?.Enhancements ?? Enumerable.Empty<IEnhancement<IStatistic>>();
+            foreach (var mod in newMods)
+            {
+                BaseStatistics.Apply(mod);
+            }
         }
 
         public void IncreaseStrength()
         {
             if (SkillPoints > 0)
             {
-                //var strength = BaseStatistics.GetStatistic<Strength>();
-                //var newStrength = new Strength(strength.Value + 1);
-                //BaseStatistics.Change(newStrength);
-                //--SkillPoints;
-                ////might not be needed
-                //var newHealth = new Health(Experience.Level, newStrength);
-                //BaseStatistics.Change(newHealth);
+                BaseStatistics.GetStatistics<Strength>().Single().LevelUp();
+                --SkillPoints;
             }
             else throw new InvalidOperationException($"Not enough {nameof(SkillPoints)}:{SkillPoints}.");
         }
@@ -86,14 +87,9 @@ namespace Odysseus.DomainModel.GameMechanics
         {
             if (level.Value > Experience.Level.Value)
             {
-                //SkillPoints += level.Value - Experience.Level.Value;
-                //var strength = BaseStatistics.GetStatistic<Strength>();
-                //var newHealth = new Health(Experience.Level, new Strength(strength.Value));
-                //BaseStatistics.Change(newHealth);
-
-                //var intelligence = BaseStatistics.GetStatistic<Intelligence>();
-                //var newMana = new Mana(Experience.Level, new Intelligence(intelligence.Value));
-                //BaseStatistics.Change(newMana);
+                SkillPoints += level.Value - Experience.Level.Value;
+                BaseStatistics.GetStatistics<Health>().Single().LevelUp();
+                BaseStatistics.GetStatistics<Mana>().Single().LevelUp();
             }
         }
     }
