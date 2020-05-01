76d5dcedfc0b5ed73dc9d27214ea23d107359a1a
Spec <dawidjachowicz91@gmail.com>
2020-05-01 14:09:39 +0200

    Add CharacterAttribute abstraction - Change attributes according to the new abstraction - Add corresponding unit tests

---
+++ src/server/DomainModel/GameMechanics/Statistics/Implementations/Attributes/CharacterAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Odysseus.DomainModel.GameMechanics.Statistics.Base;
+using System.Linq;
+
+namespace Odysseus.DomainModel.GameMechanics.Statistics.Implementations.Attributes
+{
+    public abstract class CharacterAttribute : Statistic, IPrimaryStatistic, ILevelable
+    {
+        private const int Minimum = 1;
+        private int BaseValue { get; set; } = Minimum;
+        public int Value => BaseValue + Enhancements.Sum(Enhancement => Enhancement.Enhance(BaseValue));
+
+        public void LevelUp() =>
+            BaseValue++;
+    }
+}
--- src/server/DomainModel/GameMechanics/Statistics/Implementations/Attributes/Dexterity.cs
+++ src/server/DomainModel/GameMechanics/Statistics/Implementations/Attributes/Dexterity.cs
@@ -1,17 +1,6 @@
-﻿using Odysseus.DomainModel.GameMechanics.Statistics.Base;
-using System.Linq;
-
-namespace Odysseus.DomainModel.GameMechanics.Statistics.Implementations.Attributes
+﻿namespace Odysseus.DomainModel.GameMechanics.Statistics.Implementations.Attributes
 {
-    public class Dexterity : Statistic, IPrimaryStatistic, ILevelable
+    public class Dexterity : CharacterAttribute
     {
-        private const int Minimum = 1;
-        public int Value => BaseValue + Enhancements.Sum(e => e.Enhance(BaseValue));
-        public int BaseValue { get; set; } = Minimum;
-
-        public void LevelUp()
-        {
-            BaseValue++;
-        }
     }
 }
