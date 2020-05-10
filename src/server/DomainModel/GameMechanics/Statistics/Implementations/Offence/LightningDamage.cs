299e69d44ba77c6e489ba2c58300032454dc9995
Spec <dawid.spec.jachowicz@gmail.com>
2020-05-10 22:32:52 +0200

    Add ElementalDamage and corresponding Unit Tests

--- src/server/DomainModel/GameMechanics/Statistics/Implementations/Offence/LightningDamage.cs
+++ src/server/DomainModel/GameMechanics/Statistics/Implementations/Offence/LightningDamage.cs
@@ -1,17 +1,6 @@
-﻿using Odysseus.DomainModel.GameMechanics.Statistics.Base;
-using Odysseus.DomainModel.GameMechanics.Statistics.Implementations.Attributes;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace Odysseus.DomainModel.GameMechanics.Statistics.Implementations.Offence
+﻿namespace Odysseus.DomainModel.GameMechanics.Statistics.Implementations.Offence
 {
-    public class LightningDamage : Damage
+    public class LightningDamage : ElementalDamage
     {
-        public override int DeriveValue(IEnumerable<IPrimaryStatistic> statistics)
-        {
-            var intelligence = statistics.Single(statistic => statistic is Intelligence);
-            var derivedValue = (baseValue + intelligence.Value);
-            return derivedValue + Enhancements.Sum(enhancement => enhancement.Enhance(derivedValue));
-        }
     }
 }
