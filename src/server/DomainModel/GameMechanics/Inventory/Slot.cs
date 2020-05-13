ffe2a9e90835ad58d31677edd039aab15c282113
Spec <dawidjachowicz91@gmail.com>
2020-05-13 14:00:19 +0200

    Change Slot and Equipment to use SlotType - Add SlotType enum - Add a constructor to Slot - Change Equip logic - Add constructors to EquipedEventArgs

--- src/server/DomainModel/GameMechanics/Inventory/Slot.cs
+++ src/server/DomainModel/GameMechanics/Inventory/Slot.cs
@@ -6,11 +6,19 @@ namespace Odysseus.DomainModel.GameMechanics.Inventory
     public class Slot
     {
         private IEquipable? item;
+
         public bool IsEmpty => item is null;
+        public SlotType Type { get; }
+
         public IEquipable Item
         {
-            get => item ?? throw new InvalidOperationException();
-            set => item = value;
+            get => item ?? throw new InvalidOperationException($"{nameof(Slot)} is Empty.");
+            set => item = value.SlotType == Type
+                ? value
+                : throw new InvalidOperationException($"{nameof(SlotType)} of the Item:{value.SlotType} is invalid:{Type}.");
         }
+
+        public Slot(SlotType type) =>
+            Type = type;
     }
 }
