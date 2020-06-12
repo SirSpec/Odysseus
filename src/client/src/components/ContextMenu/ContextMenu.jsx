8683b7552493a53bbcd34d270b1d54a04e694f96
Spec <dawidjachowicz91@gmail.com>
2020-06-12 13:23:29 +0200

    Add ContextMenuContainer which connects ContextMenu with Redux store

---
+++ src/client/src/components/ContextMenu/ContextMenu.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from "../Container/Container";
+
+import styles from "../GameWindow/styles";
+
+const ContextMenu = (props) => {
+  return (
+    <Container className={styles.flex1} isDark withTitle title="Inventory">
+    </Container>
+  )
+}
+
+export default ContextMenu
