6b51e3e4c222c5568400b95232ddd0b578f3a6f8
Spec <dawidjachowicz91@gmail.com>
2020-06-12 13:26:45 +0200

    Add ActivityLogContainer which connects ActivityLog with Redux store - Refactor ActivityLog.stories.jsx - Remove redundant ContextMenu.jsx file

--- src/client/src/components/ActivityLog/ActivityLog.stories.jsx
+++ src/client/src/components/ActivityLog/ActivityLog.stories.jsx
@@ -5,33 +5,33 @@ import "../../styles/global.scss";
 export default { title: "Components/ActivityLog" };
 
 const logs = [
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  "Proin ullamcorper dui quis metus euismod, vel sagittis erat mollis.",
-  "Donec eget nulla vulputate, laoreet magna at, efficitur magna.",
-  "Ut in felis at ipsum molestie laoreet.",
-  "Nam congue ligula et nulla dictum, id porttitor odio suscipit."
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    "Proin ullamcorper dui quis metus euismod, vel sagittis erat mollis.",
+    "Donec eget nulla vulputate, laoreet magna at, efficitur magna.",
+    "Ut in felis at ipsum molestie laoreet.",
+    "Nam congue ligula et nulla dictum, id porttitor odio suscipit."
 ]
 
 export const Empty = () =>
-  <ActivityLog pageSize={1} />
+    <ActivityLog pageSize={1} />
 
 export const PageSize6 = () =>
-  <ActivityLog logs={logs} pageSize={6} />
+    <ActivityLog logs={logs} pageSize={6} />
 
 export const PageSize5 = () =>
-  <ActivityLog logs={logs} pageSize={5} />
+    <ActivityLog logs={logs} pageSize={5} />
 
 export const PageSize4 = () =>
-  <ActivityLog logs={logs} pageSize={4} />
+    <ActivityLog logs={logs} pageSize={4} />
 
 export const PageSize3 = () =>
-  <ActivityLog logs={logs} pageSize={3} />
+    <ActivityLog logs={logs} pageSize={3} />
 
 export const PageSize2 = () =>
-  <ActivityLog logs={logs} pageSize={2} />
+    <ActivityLog logs={logs} pageSize={2} />
 
 export const PageSize1 = () =>
-  <ActivityLog logs={logs} pageSize={1} />
+    <ActivityLog logs={logs} pageSize={1} />
 
 export const PageSize0 = () =>
-  <ActivityLog logs={logs} pageSize={0} />
+    <ActivityLog logs={logs} pageSize={0} />
