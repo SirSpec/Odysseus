bb8b5fa5bbfbf40eb0ebaeac4c25f509fba4287e
Spec <dawidjachowicz91@gmail.com>
2020-06-27 22:45:14 +0200

    Extend PlayerCharacter and MapHub logic to test interactions - Add Attack and TakeDamage methods logic

--- src/server/DomainModel/MapGenerator/Map.cs
+++ src/server/DomainModel/MapGenerator/Map.cs
@@ -5,10 +5,17 @@ namespace Odysseus.DomainModel.MapGenerator
     public readonly struct Map
     {
         public IEnumerable<Tile> Tiles { get; }
-        
+        public Grid Grid { get; }
+
         public Map(IEnumerable<Tile> tiles)
         {
             Tiles = tiles;
+
+            Grid = new Grid();
+            foreach (var tile in tiles)
+            {
+                Grid.Add(tile);
+            }
         }
     }
 }
--- src/server/Infrastructure/WebApi/Hubs/MapHub.cs
+++ src/server/Infrastructure/WebApi/Hubs/MapHub.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using Odysseus.DomainModel.GameMechanics;
+using Odysseus.DomainModel.GameMechanics.Items;
+using Odysseus.DomainModel.GameMechanics.Items.Weapons;
 using Odysseus.DomainModel.MapGenerator;
 using Odysseus.DomainServices.MapGenerator;
 using Odysseus.Framework.Randomizer;
@@ -13,20 +15,42 @@ namespace Odysseus.Infrastructure.WebApi.Hubs
     {
         static List<PlayerCharacter> Mobs = new List<PlayerCharacter>
         {
-            new PlayerCharacter("1"),
-            new PlayerCharacter("2"),
-            new PlayerCharacter("3"),
+            new PlayerCharacter("Mob 1"),
+            new PlayerCharacter("Mob 2"),
+            new PlayerCharacter("Mob 3"),
         };
 
+        static List<dynamic> mobsPosition;
+
         static PlayerCharacter Hero = new PlayerCharacter("Hero");
+
+        static Map Map;
 
         public async Task SendMap()
         {
             var config = new AdjacentRoomsGeneratorConfiguration(10, 3, new ConstraintRange<int>(5, 10), new ConstraintRange<int>(5, 10));
             var generator = new ConnectedRoomsDungeonGenerator(new AdjacentRoomsGenerator(config), new CorridorsGenerator(0.4, 2));
+            Map = generator.Generate();
 
-            await Clients.All.SendAsync("ReceiveMap", generator.Generate());
-            await Clients.All.SendAsync("ReceiveMobs", Mobs.Select(m => m.Name));
+            mobsPosition = new List<dynamic>
+        {
+            new { x =10, y = 10 },
+            new { x =14, y = 8 },
+            new { x =3, y = 5 },
+        };
+
+            await Clients.All.SendAsync("ReceiveMap", Map);
+            await Clients.All.SendAsync("ReceiveMobsPosition", mobsPosition);
+        }
+
+        public async Task SendMobs()
+        {
+            await Clients.All.SendAsync("ReceiveMobsPosition", mobsPosition);
+        }
+
+        public async Task Scan(int x, int y)
+        {
+            await Clients.All.SendAsync("GetScaned", GetMobsToAttack(x, y));
         }
 
         public async Task Attack(string name)
@@ -37,7 +61,7 @@ namespace Odysseus.Infrastructure.WebApi.Hubs
             {
                 var dmg = Hero.Attack();
                 mob.TakeDamage(dmg);
-                await Clients.All.SendAsync("ReceiveLog", $"Hero dealt {dmg} damage to mob: {mob.Name}.");
+                await Clients.All.SendAsync("ReceiveLog", $"Hero dealt {dmg.Value} damage to mob: {mob.Name}.");
             }
 
             if (mob.HealthPool.Current == 0)
@@ -47,6 +71,38 @@ namespace Odysseus.Infrastructure.WebApi.Hubs
             }
 
             await Clients.All.SendAsync("ReceiveMobs", Mobs.Select(m => m.Name));
+            await Clients.All.SendAsync("ReceiveMobsPosition", mobsPosition);
+        }
+
+        public async Task Search(int x, int y)
+        {
+            for (int i = 0; i < mobsPosition.Count; i++)
+            {
+                var searcher = new AStarSearch(Map.Grid);
+                var path = searcher.GetPath(new Tile(mobsPosition[i].x, mobsPosition[i].y), new Tile(x, y)).ToList();
+                if(path.Count > 1)
+                    mobsPosition[i] = new { x = path[1].X, y = path[1].Y };
+            }
+
+            await Clients.All.SendAsync("ReceiveMobsPosition", mobsPosition);
+            await Clients.All.SendAsync("GetScaned", GetMobsToAttack(x, y));
+        }
+
+        static bool CanAttack(int x, int y, dynamic mobPosition)
+        {
+            var h = x == mobPosition.x || x == mobPosition.x - 1 || x == mobPosition.x + 1;
+            var v = y == mobPosition.y || y == mobPosition.y - 1 || y == mobPosition.y + 1;
+
+            return h && v;
+        }
+        
+        static IEnumerable<string> GetMobsToAttack(int x, int y)
+        {
+            for (int i = 0; i < mobsPosition.Count; i++)
+            {
+                if (CanAttack(x, y, mobsPosition[i]))
+                    yield return "Mob " + (i+1);
+            }
         }
     }
 }
