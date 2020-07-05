dd336c37f2b227d208e0c56f703347cff1878099
Spec <dawid.spec.jachowicz@gmail.com>
2020-07-05 12:46:10 +0200

    Adjust Map component to use PixiJs

--- src/client/src/components/Map/Map.jsx
+++ src/client/src/components/Map/Map.jsx
@@ -1,55 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from "prop-types";
 import * as signalR from "@microsoft/signalr"
-import Canvas from "./Canvas";
+import * as PIXI from 'pixi.js'
+import MapViewRenderer from "./MapViewRenderer";
 import InputService from "./InputService";
 import MapService from "./MapService";
 
 import styles from "../../styles/gameWindow";
 
-const DisplayOptions = {
-    tileSize: 20,
-    tilePadding: 5,
-
-    backgroundColor: '#000000', //Black
-    foregroundColor: '#ffffff', //White
-    clickedTileColor: "lightgrey",
-    tileColor: "grey",
-    actorColor: "red",
-    mobsColor: "green"
-}
-
 const Map = (props) => {
-    const [isLoading, setIsLoading] = useState(true);
-    const canvasRef = React.useRef(null);
-    const [canvas, setCanvas] = useState(null);
-    const [screenCenter, setScreenCenter] = useState({ x: 0, y: 0 });
+    const mapViewRef = useRef(null);
+    const [mapViewRenderer, setMapViewRenderer] = useState(null);
     const [connection, setConnection] = useState(null);
+    const [screenCenter, setScreenCenter] = useState({ x: 0, y: 0 });
 
     useEffect(() => {
-        var connection = new signalR.HubConnectionBuilder().withUrl("https://localhost:44301/hub").build()
+        var connection = new signalR.HubConnectionBuilder().withUrl("https://localhost:5001/hub").build()
 
         connection.start().then(() => {
             console.log("Map component connection started.")
         }).catch(err => {
-            console.log(err.toString())
+            console.log(err)
         });
 
         setConnection(connection);
-        setCanvas(new Canvas(canvasRef, DisplayOptions));
+
+        const app = new PIXI.Application({
+            width: props.canvasConfiguration.width,
+            height: props.canvasConfiguration.height,
+        });
+
+        mapViewRef.current.appendChild(app.view)
+        setMapViewRenderer(new MapViewRenderer(mapViewRef, app, props.setHoveredTile));
     }, []);
 
     useEffect(() => {
-        if (canvas && props.map.tiles && props.mobs) {
-            canvas.drawCanvas();
-            canvas.drawMap(props.map, screenCenter);
-            canvas.drawActor(props.playerPosition, screenCenter);
-            canvas.drawMobs(props.mobs, screenCenter);
-            setIsLoading(false);
+        if (mapViewRenderer && props.map.tiles && props.mobs) {
+            mapViewRenderer.drawMap(props.map, screenCenter);
+            mapViewRenderer.drawActor(props.playerPosition, screenCenter);
+            mapViewRenderer.drawMobs(props.mobs, screenCenter);
         }
-    }, [canvas, screenCenter, props.map, props.playerPosition, props.mobs]);
+    }, [screenCenter, props.map, props.playerPosition, props.mobs]);
 
-    function handler(event) {
+
+    function onKeyDownHandler(event) {
         var offset = InputService.getScreenCenterOffset(event.key);
         if (!MapService.isZero(offset)) {
             setScreenCenter(MapService.offset(screenCenter, offset))
@@ -61,7 +55,7 @@ const Map = (props) => {
             var newActor = MapService.offset(props.playerPosition, offset);
             if (MapService.contains(props.map.tiles, newActor)) {
                 props.setPlayerPosition(newActor)
-                
+
                 connection.invoke("Search", props.playerPosition.x, props.playerPosition.y).catch(err => {
                     console.log(err)
                 });
@@ -69,41 +63,29 @@ const Map = (props) => {
         }
     }
 
-    function mouseClick(clickEvent) {
-        const newLocation = canvas.mousePosition(clickEvent)
-        var tileCoords = canvas.tileCoordinates(newLocation);
+    // function mouseClick(clickEvent) {
+    //     const newLocation = canvas.mousePosition(clickEvent, mapViewRef.current)
+    //     var tileCoords = canvas.tileCoordinates(newLocation);
 
-        var relative = MapService.relativeToScreenCenter(tileCoords, screenCenter)
+    //     var relative = MapService.relativeToScreenCenter(tileCoords, screenCenter)
 
-        if (MapService.contains(props.map.tiles, relative)) {
-            if (props.selectedTile) {
-                canvas.drawFloor(props.selectedTile);
-            }
-            canvas.clickTile(clickEvent);
-            props.selectTile(tileCoords)
-        }
-    }
+    //     if (MapService.contains(props.map.tiles, relative)) {
+    //         // if (props.selectedTile) {
+    //         //     canvas.drawFloor(props.selectedTile);
+    //         // }
+    //         canvas.clickTile(clickEvent);
+    //         props.selectTile(tileCoords)
+    //     }
+    // }
 
-    function handleCanvasClick(e) {
-        if (canvas) {
-            var mouseOnCanvas = canvas.mousePosition(e)
-            props.setHoveredTile(canvas.tileCoordinates(mouseOnCanvas))
-        }
-    }
+    // function handleCanvasClick(e) {
+    //     if (canvas) {
+    //         var mouseOnCanvas = canvas.mousePosition(e)
+    //         props.setHoveredTile(canvas.tileCoordinates(mouseOnCanvas))
+    //     }
+    // }
 
-    return (
-        <canvas
-            id="map"
-            tabIndex="0"
-            className={styles.floatLeft}
-            ref={canvasRef}
-            width={props.canvasConfiguration.width}
-            height={props.canvasConfiguration.height}
-            onMouseMove={handleCanvasClick}
-            onClick={mouseClick}
-            onKeyDown={handler}
-        />
-    );
+    return (<div id="mapView" tabIndex="0" ref={mapViewRef} className={styles.floatLeft} onKeyDown={onKeyDownHandler} />);
 };
 
 Map.propTypes = {
