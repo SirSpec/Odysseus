7f7a595fec8721ae08cd9e104ee14ed107115ce1
Spec <dawid.spec.jachowicz@gmail.com>
2020-07-05 12:43:17 +0200

    Refactor MapViewConfiguration  - Remove redundant colors  - Wrap colors into object

--- src/client/src/components/Map/MapViewConfiguration.js
+++ src/client/src/components/Map/MapViewConfiguration.js
@@ -2,10 +2,10 @@ export default {
     tileSize: 20,
     tilePadding: 5,
 
-    backgroundColor: 0x000000, //Black
-    foregroundColor: 0xffffff, //White
-    clickedTileColor: 0xD3D3D3, //Light Grey,
-    tileColor: 0x808080, // Grey
-    actorColor: 0xFF0000, //Red,
-    mobsColor: 0x008000// Green
+    colors: {
+        clickedTileColor: 0xD3D3D3, //Light Grey,
+        tileColor: 0x808080, // Grey
+        actorColor: 0xFF0000, //Red,
+        mobsColor: 0x008000// Green
+    }
 }
