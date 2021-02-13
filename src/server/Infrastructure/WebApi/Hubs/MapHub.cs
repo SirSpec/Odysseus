db98338876221145f60d0a03ec9a8ec4be542354
Spec <dawid.spec.jachowicz@gmail.com>
2021-02-13 08:54:04 +0100

    Remove GameMechanics from MapHub

--- src/server/Infrastructure/WebApi/Hubs/MapHub.cs
+++ src/server/Infrastructure/WebApi/Hubs/MapHub.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.SignalR;
-using Odysseus.DomainModel.GameMechanics;
 using Odysseus.DomainModel.MapGenerator;
 using Odysseus.DomainServices.MapGenerator;
 using Odysseus.Framework.Randomizer;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,9 +9,6 @@ namespace Odysseus.Infrastructure.WebApi.Hubs
 {
     public static class GameContext
     {
-        public static PlayerCharacter Hero = new PlayerCharacter("Hero");
-        public static PlayerCharacter Enemy = new PlayerCharacter("Enemy");
-
         public static (int x, int y) EnemyPosition = (10, 10);
 
         public static Map Map;
@@ -26,36 +21,6 @@ namespace Odysseus.Infrastructure.WebApi.Hubs
         }
     }
 
-    public class BattleManager
-    {
-        public void Attack()
-        {
-            var dmg = GameContext.Hero.Attack();
-            GameContext.Enemy.TakeDamage(dmg);
-        }
-
-        public async Task Search(int x, int y)
-        {
-            var searcher = new AStarSearch(GameContext.Map.Grid);
-            var path = searcher.GetPath(new Tile(GameContext.EnemyPosition.x, GameContext.EnemyPosition.y), new Tile(x, y)).ToList();
-            if (path.Count > 1) GameContext.EnemyPosition = (path[1].X, path[1].Y);
-        }
-
-        static bool CanAttack(int x, int y, (int x, int y) mobPosition)
-        {
-            var h = x == mobPosition.x || x == mobPosition.x - 1 || x == mobPosition.x + 1;
-            var v = y == mobPosition.y || y == mobPosition.y - 1 || y == mobPosition.y + 1;
-
-            return h && v;
-        }
-
-        static IEnumerable<string> GetMobsToAttack(int x, int y)
-        {
-            if (CanAttack(x, y, GameContext.EnemyPosition))
-                yield return GameContext.Enemy.Name;
-        }
-    }
-
     public class MapHub : Hub
     {
         public async Task SendMap()
@@ -64,16 +29,6 @@ namespace Odysseus.Infrastructure.WebApi.Hubs
             await Clients.All.SendAsync("ReceiveMobsPosition", new { GameContext.EnemyPosition.x, GameContext.EnemyPosition.y });
         }
 
-        public async Task Attack()
-        {
-            var dmg = GameContext.Hero.Attack();
-            GameContext.Enemy.TakeDamage(dmg);
-            await Clients.All.SendAsync("ReceiveLog", $"Hero dealt {dmg.Value} damage to Enemy: {GameContext.Enemy.Name}.");
-
-            if (GameContext.Enemy.HealthPool.Current == 0)
-                await Clients.All.SendAsync("ReceiveLog", $"Hero killed mob: {GameContext.Enemy.Name}.");
-        }
-
         public async Task Search(int x, int y)
         {
             var searcher = new AStarSearch(GameContext.Map.Grid);
@@ -81,7 +36,6 @@ namespace Odysseus.Infrastructure.WebApi.Hubs
             if (path.Count > 1) GameContext.EnemyPosition = (path[1].X, path[1].Y);
 
             await Clients.All.SendAsync("ReceiveMobsPosition", new { GameContext.EnemyPosition.x, GameContext.EnemyPosition.y });
-            await Clients.All.SendAsync("GetScaned", GetMobsToAttack(x, y));
         }
 
         static bool CanAttack(int x, int y, (int x, int y) mobPosition)
@@ -90,12 +44,6 @@ namespace Odysseus.Infrastructure.WebApi.Hubs
             var v = y == mobPosition.y || y == mobPosition.y - 1 || y == mobPosition.y + 1;
 
             return h && v;
-        }
-
-        static IEnumerable<string> GetMobsToAttack(int x, int y)
-        {
-            if (CanAttack(x, y, GameContext.EnemyPosition))
-                yield return GameContext.Enemy.Name;
         }
     }
 }
