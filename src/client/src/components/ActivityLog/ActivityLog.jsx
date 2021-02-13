3a7a81f67a81e413cb6aeb1ad29864bdfd2dab9c
Spec <dawid.spec.jachowicz@gmail.com>
2021-02-13 19:40:55 +0100

    Add hoveredTile info to ActivityLog component

--- src/client/src/components/ActivityLog/ActivityLog.jsx
+++ src/client/src/components/ActivityLog/ActivityLog.jsx
@@ -36,7 +36,7 @@ const ActivityLog = (props) => {
             {logs().map((log, index) => <p key={index}>{log}</p>)}
             <Span onClick={handleDecrementPage}>{'<'}</Span>
             <Span> {pageNumber} </Span>
-            <Span onClick={handleIncrementPage}>{'>'}</Span>
+            <Span onClick={handleIncrementPage}>{'>'} {props.hoveredTile?.x} {props.hoveredTile?.y}</Span>
         </Container>
     );
 };
