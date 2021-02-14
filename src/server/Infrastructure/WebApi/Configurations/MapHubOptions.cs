fa0366b44c632839cb91fdccc69d3969214bdb02
Spec <dawidjachowicz91@gmail.com>
2021-02-15 00:02:43 +0100

    Refactor WebApi

---
+++ src/server/Infrastructure/WebApi/Configurations/MapHubOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Odysseus.Infrastructure.WebApi.Configurations
+{
+    public class MapHubOptions
+    {
+        public const string MapHub = "MapHub";
+
+        public string? Url { get; set; }
+    }
+}
